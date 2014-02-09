05093ffafc8fafcde25582ffc668e16747135a9a
Ishan Khanna <boilingstocks@gmail.com>
2014-02-10 03:54:06 +0530

    Updated Todo

--- MifosXDroid/src/main/java/com/mifos/mifosxdroid/DashboardFragmentActivity.java
+++ MifosXDroid/src/main/java/com/mifos/mifosxdroid/DashboardFragmentActivity.java
@@ -26,6 +26,16 @@ import java.util.List;
 /**
  * Created by ishankhanna on 09/02/14.
  */
+
+/**
+
+ TODO
+ 1. Use SharedPreferences to Get authenticationKey, instead of Intents : Use PreferenceManager, SharedPreferences
+ 2. Use Asynchronous loading of clients instead of performing Network Calls on main thread : Use Callback
+ 3. Create Separate fragment Class instead of using the PlaceHolderFragment : Use Fragment
+ 4. Format the ListView to show Names more neatly : Use ListAdapter
+ */
+
 public class DashboardFragmentActivity extends ActionBarActivity{
 
     //TODO Remove this Sample Key
--- ToDo.md
+++ ToDo.md
@@ -1,4 +1,10 @@
 Task List
 ------------------------
 
+ 1. Use SharedPreferences to Get authenticationKey, instead of Intents : Use PreferenceManager, SharedPreferences
+ 2. Use Asynchronous loading of clients instead of performing Network Calls on main thread : Use Callback
+ 3. Create Separate fragment Class instead of using the PlaceHolderFragment : Use Fragment
+ 4. Format the ListView to show Names more neatly : Use ListAdapter
+
+
 
