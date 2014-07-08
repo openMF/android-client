59c36e9dce67e2a46a3266052a03e032cfb21593
Ishan Khanna <boilingstocks@gmail.com>
2014-07-08 13:35:05 +0530

    MIFOSX 1406.7 Fixed

--- mifosng-android/src/main/java/com/mifos/mifosxdroid/online/ClientSearchFragment.java
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/online/ClientSearchFragment.java
@@ -37,26 +37,27 @@ public class ClientSearchFragment extends Fragment implements AdapterView.OnItem
 
     private static final String TAG = "Client Search Fragment";
 
-    @InjectView(R.id.et_search_by_id) EditText et_searchById;
-    @InjectView(R.id.bt_searchClient) Button bt_searchClient;
-    @InjectView(R.id.lv_searchResults) ListView lv_searchResults;
+    @InjectView(R.id.et_search_by_id)
+    EditText et_searchById;
+    @InjectView(R.id.bt_searchClient)
+    Button bt_searchClient;
+    @InjectView(R.id.lv_searchResults)
+    ListView lv_searchResults;
 
     View rootView;
-
-    private String searchQuery;
-
     List<String> clientNames = new ArrayList<String>();
     List<Integer> clientIds = new ArrayList<Integer>();
-
     SafeUIBlockingUtility safeUIBlockingUtility;
+    private String searchQuery;
 
+
+    public ClientSearchFragment() {
+        // Required empty public constructor
+    }
 
     public static ClientSearchFragment newInstance() {
         ClientSearchFragment fragment = new ClientSearchFragment();
         return fragment;
-    }
-    public ClientSearchFragment() {
-        // Required empty public constructor
     }
 
     @Override
@@ -68,25 +69,22 @@ public class ClientSearchFragment extends Fragment implements AdapterView.OnItem
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.fragment_client_search,null);
+        rootView = inflater.inflate(R.layout.fragment_client_search, null);
         ButterKnife.inject(this, rootView);
-
 
 
         return rootView;
     }
 
     @OnClick(R.id.bt_searchClient)
-    public void performSearch(){
+    public void performSearch() {
 
-        if(!et_searchById.getEditableText().toString().trim().isEmpty())
-        {
+        if (!et_searchById.getEditableText().toString().trim().isEmpty()) {
             searchQuery = et_searchById.getEditableText().toString().trim();
             findClients(searchQuery);
 
-        }else
-        {
-            Toast.makeText(getActivity(),"No Search Query Entered!",Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getActivity(), "No Search Query Entered!", Toast.LENGTH_SHORT).show();
         }
 
 
@@ -97,23 +95,22 @@ public class ClientSearchFragment extends Fragment implements AdapterView.OnItem
 
         safeUIBlockingUtility = new SafeUIBlockingUtility(getActivity());
         safeUIBlockingUtility.safelyBlockUI();
-        API.searchService.searchClientsByName(clientName,new Callback<List<SearchedEntity>>() {
+        API.searchService.searchClientsByName(clientName, new Callback<List<SearchedEntity>>() {
             @Override
             public void success(List<SearchedEntity> searchedEntities, Response response) {
 
                 Iterator<SearchedEntity> iterator = searchedEntities.iterator();
-                Log.i(TAG, "Results Found = "+searchedEntities.size());
-                while(iterator.hasNext())
-                {
+                Log.i(TAG, "Results Found = " + searchedEntities.size());
+                while (iterator.hasNext()) {
                     SearchedEntity searchedEntity = iterator.next();
-                    clientNames.add(searchedEntity.getEntityName());
+                    clientNames.add("#" + searchedEntity.getEntityId() + " - " + searchedEntity.getEntityName());
                     clientIds.add(searchedEntity.getEntityId());
                 }
 
                 String[] clientNamesArrayForAdapter = clientNames.toArray(new String[clientNames.size()]);
 
                 ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-                        android.R.layout.simple_list_item_1,clientNamesArrayForAdapter);
+                        android.R.layout.simple_list_item_1, clientNamesArrayForAdapter);
 
                 lv_searchResults.setAdapter(adapter);
                 lv_searchResults.setOnItemClickListener(ClientSearchFragment.this);
@@ -136,8 +133,6 @@ public class ClientSearchFragment extends Fragment implements AdapterView.OnItem
         });
 
 
-
-
     }
 
 
@@ -149,7 +144,7 @@ public class ClientSearchFragment extends Fragment implements AdapterView.OnItem
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-        Intent clientActivityIntent = new Intent(getActivity(),ClientActivity.class);
+        Intent clientActivityIntent = new Intent(getActivity(), ClientActivity.class);
         clientActivityIntent.putExtra(Constants.CLIENT_ID, clientIds.get(i));
         startActivity(clientActivityIntent);
 
