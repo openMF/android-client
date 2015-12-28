16762bebf96c5b521b2c5a5ba5df247f8756ee62
chhavip <coolaqua12@gmail.com>
2015-12-28 12:08:27 +0530

    Offline Mode Crash Fix

--- mifosng-android/src/main/java/com/mifos/mifosxdroid/OfflineCenterInputActivity.java
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/OfflineCenterInputActivity.java
@@ -47,7 +47,7 @@ public class OfflineCenterInputActivity extends ActionBarActivity implements Dat
         }
         setContentView(R.layout.activity_center_details);
         ButterKnife.inject(this);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
 
