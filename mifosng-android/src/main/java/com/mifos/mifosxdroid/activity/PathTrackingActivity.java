a31d152c372075764b1b6b13a76a7e088b471b7c
Vishwajeet Srivastava <vishwajeets912@gmail.com>
2016-03-13 04:11:51 +0530

    MIFOSAC-121 back button added on toolbar in Track my path activity

--- mifosng-android/src/main/java/com/mifos/mifosxdroid/activity/PathTrackingActivity.java
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/activity/PathTrackingActivity.java
@@ -54,6 +54,8 @@ public class PathTrackingActivity extends MifosBaseActivity implements ServiceCo
                 service.stopLogging();
             }
         });
+
+        showBackButton();
     }
 
     @Override
