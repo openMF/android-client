0dbc97d3f864e07f852433b94ca47a48b5be79a5
therajanmaurya <rajanmaurya154@gmail.com>
2016-05-24 22:36:44 +0530

    CollapsibleIfStatements

--- mifosng-android/src/main/java/com/mifos/mifosxdroid/core/EndlessRecyclerOnScrollListener.java
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/core/EndlessRecyclerOnScrollListener.java
@@ -7,7 +7,7 @@ import android.support.v7.widget.RecyclerView;
  * Created by rajanmaurya on 16/4/2016.
  */
 public abstract class EndlessRecyclerOnScrollListener extends RecyclerView.OnScrollListener {
-    public static String TAG = EndlessRecyclerOnScrollListener.class.getSimpleName();
+    public static final String TAG = EndlessRecyclerOnScrollListener.class.getSimpleName();
     int firstVisibleItem, visibleItemCount, totalItemCount;
     private int previousTotal = 0; // The total number of items in the dataset after the last load
     private boolean loading = true; // True if we are still waiting for the last set of data to
@@ -30,12 +30,11 @@ public abstract class EndlessRecyclerOnScrollListener extends RecyclerView.OnScr
         totalItemCount = mLinearLayoutManager.getItemCount();
         firstVisibleItem = mLinearLayoutManager.findFirstVisibleItemPosition();
 
-        if (loading) {
-            if (totalItemCount > previousTotal + 1) {
+        if (loading && (totalItemCount > previousTotal + 1)) {
                 loading = false;
                 previousTotal = totalItemCount;
-            }
         }
+
         if (!loading && (totalItemCount - visibleItemCount) <= (firstVisibleItem +
                 visibleThreshold)) {
             // End has been reached
