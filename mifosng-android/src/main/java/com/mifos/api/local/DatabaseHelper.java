3b27f717385f53ddccd3d506bfbdfcc3c3974961
Rajan Maurya <rajanmaurya154@gmail.com>
2016-06-23 17:44:28 +0530

    DatabaseHelper added .

---
+++ mifosng-android/src/main/java/com/mifos/api/local/DatabaseHelper.java
@@ -0,0 +1,18 @@
+package com.mifos.api.local;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * Created by Rajan Maurya on 23/06/16.
+ */
+@Singleton
+public class DatabaseHelper {
+
+    @Inject
+    public DatabaseHelper() {
+
+    }
+
+
+}
