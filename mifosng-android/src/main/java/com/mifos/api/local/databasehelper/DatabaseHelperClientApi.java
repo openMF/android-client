2be07a50aa6174b2acd41957ae9dc772c49b9663
Rajan Maurya <rajanmaurya154@gmail.com>
2016-06-25 16:08:23 +0530

    stetho added and Saving Clients in DBFLow

--- mifosng-android/src/main/java/com/mifos/api/local/databasehelper/DatabaseHelperClientApi.java
+++ mifosng-android/src/main/java/com/mifos/api/local/databasehelper/DatabaseHelperClientApi.java
@@ -1,5 +1,8 @@
 package com.mifos.api.local.databasehelper;
 
+import com.mifos.objects.client.Client;
+import com.mifos.objects.client.Page;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -14,4 +17,14 @@ public class DatabaseHelperClientApi {
 
     }
 
+    public void saveAllClients(Page<Client> clientPage) {
+
+        for (Client client : clientPage.getPageItems()) {
+            Client dbClient = new Client();
+            dbClient.setId(client.getId());
+            dbClient.setAccountNo(client.getAccountNo());
+            dbClient.setActive(client.isActive());
+            dbClient.save();
+        }
+    }
 }
