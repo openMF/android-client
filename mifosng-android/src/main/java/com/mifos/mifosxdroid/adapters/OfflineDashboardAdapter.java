093ee7b741cf5992e77e7f5fbcfcf4e9e56329ef
Rajan Maurya <rajanmaurya154@gmail.com>
2016-07-21 12:08:10 +0530

    OfflineDashboardAdpter done

--- mifosng-android/src/main/java/com/mifos/mifosxdroid/adapters/OfflineDashboardAdapter.java
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/adapters/OfflineDashboardAdapter.java
@@ -8,6 +8,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.mifos.mifosxdroid.R;
+import com.mifos.objects.client.ClientPayload;
+import com.mifos.objects.group.GroupPayload;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -20,11 +25,16 @@ import butterknife.ButterKnife;
 public class OfflineDashboardAdapter  extends
         RecyclerView.Adapter<OfflineDashboardAdapter.ViewHolder> {
 
+    private List<ClientPayload> clientPayloads = new ArrayList<>();
+    private List<GroupPayload> groupPayloads = new ArrayList<>();
+
+    private String [] payloadNames = {"Sync Clients", "Sync Groups"};
+    private String [] payloadCounts = {"Total Payload : " + String.valueOf(clientPayloads.size()),
+                                       "Total Payload : " + String.valueOf(groupPayloads.size())};
 
 
     @Inject
     public OfflineDashboardAdapter() {
-
     }
 
     @Override
@@ -36,16 +46,22 @@ public class OfflineDashboardAdapter  extends
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.tv_payload_name.setText("Client");
-        holder.tv_item_count.setText("3");
+        holder.tv_payload_name.setText(payloadNames[position]);
+        holder.tv_item_count.setText(payloadCounts[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 5;
+        return payloadNames.length;
     }
 
+    public void setClientPayloads(List<ClientPayload> clientPayload) {
+        clientPayloads = clientPayload;
+    }
 
+    public void setGroupPayloads(List<GroupPayload> groupPayload) {
+        groupPayloads = groupPayload;
+    }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
