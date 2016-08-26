f7a7e4a1ba8c639d54ff4ff2d650e26aade1246c
Rajan Maurya <rajanmaurya154@gmail.com>
2016-08-26 14:21:56 +0530

    GroupWithAssociation refactored

--- mifosng-android/src/main/java/com/mifos/objects/group/GroupWithAssociations.java
+++ mifosng-android/src/main/java/com/mifos/objects/group/GroupWithAssociations.java
@@ -5,33 +5,79 @@
 
 package com.mifos.objects.group;
 
-import com.mifos.objects.client.Status;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
 import com.mifos.objects.Timeline;
 import com.mifos.objects.client.Client;
+import com.mifos.objects.client.Status;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by ishankhanna on 29/06/14.
  */
-public class GroupWithAssociations {
+public class GroupWithAssociations implements Parcelable {
 
-    List<Integer> activationDate = new ArrayList<Integer>();
-    Boolean active;
-    String hierarchy;
+    @SerializedName("id")
     Integer id;
+
+    @SerializedName("accountNo")
+    String accountNo;
+
+    @SerializedName("name")
     String name;
-    Integer officeId;
-    String officeName;
-    Integer staffId;
-    String staffName;
+
+    @SerializedName("status")
     Status status;
+
+    @SerializedName("active")
+    Boolean active;
+
+    @SerializedName("activationDate")
+    List<Integer> activationDate = new ArrayList<>();
+
+    @SerializedName("officeId")
+    Integer officeId;
+
+    @SerializedName("officeName")
+    String officeName;
+
+    @SerializedName("staffId")
+    Integer staffId;
+
+    @SerializedName("staffName")
+    String staffName;
+
+    @SerializedName("hierarchy")
+    String hierarchy;
+
+    @SerializedName("groupLevel")
+    Integer groupLevel;
+
+    @SerializedName("clientMembers")
+    List<Client> clientMembers = new ArrayList<>();
+
+    @SerializedName("timeline")
     Timeline timeline;
-    List<Client> clientMembers = new ArrayList<Client>();
-    Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public Integer getGroupLevel() {
+        return groupLevel;
+    }
+
+    public void setGroupLevel(Integer groupLevel) {
+        this.groupLevel = groupLevel;
+    }
 
     public List<Integer> getActivationDate() {
         return activationDate;
@@ -129,11 +175,81 @@ public class GroupWithAssociations {
         this.clientMembers = clientMembers;
     }
 
-    public Map<String, Object> getAdditionalProperties() {
-        return additionalProperties;
+    @Override
+    public String toString() {
+        return "GroupWithAssociations{" +
+                "id=" + id +
+                ", accountNo='" + accountNo + '\'' +
+                ", name='" + name + '\'' +
+                ", status=" + status +
+                ", active=" + active +
+                ", activationDate=" + activationDate +
+                ", officeId=" + officeId +
+                ", officeName='" + officeName + '\'' +
+                ", staffId=" + staffId +
+                ", staffName='" + staffName + '\'' +
+                ", hierarchy='" + hierarchy + '\'' +
+                ", groupLevel=" + groupLevel +
+                ", clientMembers=" + clientMembers +
+                ", timeline=" + timeline +
+                '}';
     }
 
-    public void setAdditionalProperties(Map<String, Object> additionalProperties) {
-        this.additionalProperties = additionalProperties;
+
+    @Override
+    public int describeContents() {
+        return 0;
     }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(this.id);
+        dest.writeString(this.accountNo);
+        dest.writeString(this.name);
+        dest.writeParcelable(this.status, flags);
+        dest.writeValue(this.active);
+        dest.writeList(this.activationDate);
+        dest.writeValue(this.officeId);
+        dest.writeString(this.officeName);
+        dest.writeValue(this.staffId);
+        dest.writeString(this.staffName);
+        dest.writeString(this.hierarchy);
+        dest.writeValue(this.groupLevel);
+        dest.writeTypedList(this.clientMembers);
+        dest.writeParcelable(this.timeline, flags);
+    }
+
+    public GroupWithAssociations() {
+    }
+
+    protected GroupWithAssociations(Parcel in) {
+        this.id = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.accountNo = in.readString();
+        this.name = in.readString();
+        this.status = in.readParcelable(Status.class.getClassLoader());
+        this.active = (Boolean) in.readValue(Boolean.class.getClassLoader());
+        this.activationDate = new ArrayList<>();
+        in.readList(this.activationDate, Integer.class.getClassLoader());
+        this.officeId = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.officeName = in.readString();
+        this.staffId = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.staffName = in.readString();
+        this.hierarchy = in.readString();
+        this.groupLevel = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.clientMembers = in.createTypedArrayList(Client.CREATOR);
+        this.timeline = in.readParcelable(Timeline.class.getClassLoader());
+    }
+
+    public static final Parcelable.Creator<GroupWithAssociations> CREATOR =
+            new Parcelable.Creator<GroupWithAssociations>() {
+        @Override
+        public GroupWithAssociations createFromParcel(Parcel source) {
+            return new GroupWithAssociations(source);
+        }
+
+        @Override
+        public GroupWithAssociations[] newArray(int size) {
+            return new GroupWithAssociations[size];
+        }
+    };
 }
