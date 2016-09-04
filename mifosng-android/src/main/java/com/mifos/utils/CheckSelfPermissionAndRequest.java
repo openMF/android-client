a8c55415b1a4940183e8e7b8203ef54a7b8f2dee
Rajan Maurya <rajanmaurya154@gmail.com>
2016-09-04 18:02:26 +0530

    if user denied the permission, then show material dialog to accept the permission again (cherry picked from commit ad4f0bd)

--- mifosng-android/src/main/java/com/mifos/utils/CheckSelfPermissionAndRequest.java
+++ mifosng-android/src/main/java/com/mifos/utils/CheckSelfPermissionAndRequest.java
@@ -2,11 +2,15 @@ package com.mifos.utils;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+
+import com.mifos.mifosxdroid.R;
+import com.mifos.mifosxdroid.core.MaterialDialog;
 
 /**
  *
@@ -21,15 +25,29 @@ public class CheckSelfPermissionAndRequest {
     }
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    public static void requestPermission(AppCompatActivity activity,
-                                         String permission,
-                                         int MY_PERMISSIONS_REQUEST) {
+    public static void requestPermission(final AppCompatActivity activity,
+                                         final String permission,
+                                         final int MY_PERMISSIONS_REQUEST,
+                                         final String dialogMessage) {
         // Should we show an explanation?
         if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
 
             // Show an expanation to the user *asynchronously* -- don't block
             // this thread waiting for the user's response! After the user
-            // sees the explanation, try again to request the permission.
+            new MaterialDialog.Builder().init(activity)
+                    .setTitle(R.string.permission_denied)
+                    .setMessage(dialogMessage)
+                    .setPositiveButton(R.string.dialog_action_i_am_sure, null)
+                    .setNegativeButton(R.string.dialog_action_re_try,
+                            new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(activity, new String[]{permission},
+                                    MY_PERMISSIONS_REQUEST);
+                        }
+                    })
+                    .createMaterialDialog()
+                    .show();            // sees the explanation, try again to request the permission.
 
         } else {
 
