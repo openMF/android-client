b3b4c8139ed0f08628462a565b4ad0f5d62a3345
Rajan Maurya <rajanmaurya154@gmail.com>
2016-09-24 13:59:17 +0530

    refactor: CenterListAdapter Enhanced

--- mifosng-android/src/main/java/com/mifos/mifosxdroid/adapters/CentersListAdapter.java
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/adapters/CentersListAdapter.java
@@ -6,7 +6,6 @@
 package com.mifos.mifosxdroid.adapters;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,7 +15,10 @@ import android.widget.TextView;
 import com.mifos.mifosxdroid.R;
 import com.mifos.objects.group.Center;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,52 +26,54 @@ import butterknife.ButterKnife;
 /**
  * Created by ishankhanna on 11/03/14.
  */
-public class CentersListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class CentersListAdapter extends RecyclerView.Adapter<CentersListAdapter.ViewHolder> {
 
-    private LayoutInflater layoutInflater;
     private List<Center> centers;
-    private Resources resources;
+    private Context context;
 
-    public CentersListAdapter(Context context, List<Center> centers) {
+    @Inject
+    public CentersListAdapter() {
+        centers = new ArrayList<>();
+    }
 
-        layoutInflater = LayoutInflater.from(context);
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row_center_list_item, parent, false);
+        return new CentersListAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+            String centerId = context.getResources()
+                    .getString(R.string.center_id) + centers.get(position).getId();
+
+            String staffId = context.getResources()
+                    .getString(R.string.staff_id) + centers.get(position).getStaffId();
+
+            String officeId = context.getResources()
+                    .getString(R.string.office_id) + centers.get(position).getOfficeId();
+
+            holder.tv_center_id.setText(centerId);
+            holder.tv_center_name.setText(centers.get(position).getName());
+            holder.tv_staff_id.setText(staffId);
+            holder.tv_staff_name.setText(centers.get(position).getStaffName());
+            holder.tv_office_id.setText(officeId);
+            holder.tv_office_name.setText(centers.get(position).getOfficeName());
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public void setCenters(List<Center> centers) {
         this.centers = centers;
-        resources = context.getResources();
-
+        notifyDataSetChanged();
     }
 
-    public Center getItem(int i) {
-        return this.centers.get(i);
-    }
-
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        RecyclerView.ViewHolder vh;
-        View v = LayoutInflater.from(parent.getContext()).inflate(
-                R.layout.row_center_list_item, parent, false);
-        vh = new ViewHolder(v);
-        return vh;
-    }
-
-    @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        if (holder instanceof ViewHolder) {
-
-            ((ViewHolder) holder).tv_center_id.setText(resources
-                    .getString(R.string.center_id) + centers.get(position).getId());
-
-            ((ViewHolder) holder).tv_center_name.setText(centers.get(position).getName());
-
-            ((ViewHolder) holder).tv_staff_id.setText(
-                    resources.getString(R.string.staff_id) + centers.get(position).getStaffId());
-
-            ((ViewHolder) holder).tv_staff_name.setText(centers.get(position).getStaffName());
-
-            ((ViewHolder) holder).tv_office_id.setText(
-                    resources.getString(R.string.office_id) + centers.get(position).getOfficeId());
-
-            ((ViewHolder) holder).tv_office_name.setText(centers.get(position).getOfficeName());
-        }
+    public Center getItem(int position) {
+        return this.centers.get(position);
     }
 
     @Override
