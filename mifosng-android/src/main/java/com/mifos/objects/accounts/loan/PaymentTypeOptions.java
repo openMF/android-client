05d62ddfefe215f1fb80580472bd44d0fe6797fe
AkshGautam <gaksh007@gmail.com>
2018-06-18 17:24:08 +0530

    Fix #979:Enhancing Individual collection sheet

--- mifosng-android/src/main/java/com/mifos/objects/accounts/loan/PaymentTypeOptions.java
+++ mifosng-android/src/main/java/com/mifos/objects/accounts/loan/PaymentTypeOptions.java
@@ -1,9 +1,12 @@
 package com.mifos.objects.accounts.loan;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by nellyk on 3/3/2016.
  */
-public class PaymentTypeOptions {
+public class PaymentTypeOptions implements Parcelable {
     int id;
     String name;
 
@@ -27,4 +30,34 @@ public class PaymentTypeOptions {
     public String toString() {
         return "id" + id + " name:" + name;
     }
+
+    protected PaymentTypeOptions(Parcel in) {
+        id = in.readInt();
+        name = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(name);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<PaymentTypeOptions> CREATOR =
+            new Parcelable.Creator<PaymentTypeOptions>() {
+        @Override
+        public PaymentTypeOptions createFromParcel(Parcel in) {
+            return new PaymentTypeOptions(in);
+        }
+
+        @Override
+        public PaymentTypeOptions[] newArray(int size) {
+            return new PaymentTypeOptions[size];
+        }
+    };
 }
