dc03dd64bd19a02ad0bb1c94a00dcb75875ce0e6
moksh-mahajan <mokshmahajan008@gmail.com>
2019-03-07 16:48:58 +0530

    Removed redundant type casting from various java files.

--- mifosng-android/src/main/java/com/mifos/mifosxdroid/core/ProgressableDialogFragment.java
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/core/ProgressableDialogFragment.java
@@ -26,7 +26,7 @@ public class ProgressableDialogFragment extends DialogFragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        viewFlipper = (ViewFlipper) view.findViewById(R.id.view_flipper);
+        viewFlipper = view.findViewById(R.id.view_flipper);
 
         if (viewFlipper == null) {
             throw new NullPointerException("Are you sure your Fragment has a ViewFlipper with id " +
