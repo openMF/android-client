3f9cd37adcb167f7ee28288f56ab58c7415a977a
RajaVamsi11 <rajavamsi1998@gmail.com>
2020-03-12 16:41:25 +0530

    Replaced .commit() with .apply() to Shared Preference objects

--- mifosng-android/src/main/java/com/mifos/utils/PrefManager.java
+++ mifosng-android/src/main/java/com/mifos/utils/PrefManager.java
@@ -35,7 +35,7 @@ public class PrefManager {
     }
 
     public static void clearPrefs() {
-        getPreferences().edit().clear().commit();
+        getPreferences().edit().clear().apply();
     }
 
     public static int getInt(String preferenceKey, int preferenceDefaultValue) {
@@ -43,7 +43,7 @@ public class PrefManager {
     }
 
     public static void putInt(String preferenceKey, int preferenceValue) {
-        getPreferences().edit().putInt(preferenceKey, preferenceValue).commit();
+        getPreferences().edit().putInt(preferenceKey, preferenceValue).apply();
     }
 
     public static long getLong(String preferenceKey, long preferenceDefaultValue) {
@@ -51,7 +51,7 @@ public class PrefManager {
     }
 
     public static void putLong(String preferenceKey, long preferenceValue) {
-        getPreferences().edit().putLong(preferenceKey, preferenceValue).commit();
+        getPreferences().edit().putLong(preferenceKey, preferenceValue).apply();
     }
 
     public static float getFloat(String preferenceKey, float preferenceDefaultValue) {
@@ -59,7 +59,7 @@ public class PrefManager {
     }
 
     public static void putFloat(String preferenceKey, float preferenceValue) {
-        getPreferences().edit().putFloat(preferenceKey, preferenceValue).commit();
+        getPreferences().edit().putFloat(preferenceKey, preferenceValue).apply();
     }
 
     public static boolean getBoolean(String preferenceKey, boolean preferenceDefaultValue) {
@@ -67,7 +67,7 @@ public class PrefManager {
     }
 
     public static void putBoolean(String preferenceKey, boolean preferenceValue) {
-        getPreferences().edit().putBoolean(preferenceKey, preferenceValue).commit();
+        getPreferences().edit().putBoolean(preferenceKey, preferenceValue).apply();
     }
 
     public static String getString(String preferenceKey, String preferenceDefaultValue) {
@@ -75,11 +75,11 @@ public class PrefManager {
     }
 
     public static void putString(String preferenceKey, String preferenceValue) {
-        getPreferences().edit().putString(preferenceKey, preferenceValue).commit();
+        getPreferences().edit().putString(preferenceKey, preferenceValue).apply();
     }
 
     public static void putStringSet(String preferencesKey, Set<String> values) {
-        getPreferences().edit().putStringSet(preferencesKey, values).commit();
+        getPreferences().edit().putStringSet(preferencesKey, values).apply();
     }
 
     public static Set<String> getStringSet(String preferencesKey) {
