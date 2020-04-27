85972e7f94dd4fb8e521180cb02b5cfa0fe0551d
RajaVamsi11 <rajavamsi1998@gmail.com>
2020-04-27 17:42:52 +0530

    Added CreateNewClient Fragment UI test

--- mifosng-android/src/instrumentTest/java/com/mifos/mifosxdroid/tests/CreateNewClientFragmentTest.java
+++ mifosng-android/src/instrumentTest/java/com/mifos/mifosxdroid/tests/CreateNewClientFragmentTest.java
@@ -1,146 +1,231 @@
-/*
- * This project is licensed under the open source MPL V2.
- * See https://github.com/openMF/android-client/blob/master/LICENSE.md
- */
-
 package com.mifos.mifosxdroid.tests;
 
-import android.test.ActivityInstrumentationTestCase2;
-import android.test.suitebuilder.annotation.MediumTest;
-import android.test.suitebuilder.annotation.SmallTest;
-import android.view.View;
-import android.widget.CheckBox;
-import android.widget.TextView;
+import android.view.KeyEvent;
+import android.widget.Spinner;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.rule.ActivityTestRule;
 
 import com.mifos.mifosxdroid.R;
-import com.mifos.mifosxdroid.online.createnewclient.CreateNewClientFragment;
 import com.mifos.mifosxdroid.online.DashboardActivity;
+import com.mifos.mifosxdroid.online.createnewclient.CreateNewClientFragment;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertTrue;
 
-/**
- * Created by ahmed fathy on 26/03/16.
- */
-public class CreateNewClientFragmentTest extends
-        ActivityInstrumentationTestCase2<DashboardActivity> {
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class CreateNewClientFragmentTest {
 
-    /* fields */
-    private DashboardActivity dashboardActivity;
-    private CreateNewClientFragment createNewClientFragment;
+    private ActivityScenario activityScenario;
+    @Rule
+    public ActivityTestRule<DashboardActivity> activityTestRule =
+            new ActivityTestRule<>(DashboardActivity.class);
 
-    /* views */
-    private TextView textViewDateOfBirth;
-    private TextView textViewSubmissionDate;
-    private CheckBox checkBoxActive;
-
-    public CreateNewClientFragmentTest() {
-        super(DashboardActivity.class);
-    }
-
-    @Override
+    @Before
     public void setUp() throws Exception {
-        super.setUp();
+        activityScenario = ActivityScenario.launch(DashboardActivity.class);
+        openCreateClient();
+    }
 
-        // open create client tab
-        dashboardActivity = getActivity();
-        dashboardActivity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                dashboardActivity.openCreateClient();
-            }
-        });
-        Thread.sleep(5000);
+    @Test
+    public void testViewsAreVisible() throws InterruptedException {
 
-        // reference views
-        createNewClientFragment = (CreateNewClientFragment) dashboardActivity
+        Thread.sleep(3000);
+
+        onView(withId(R.id.et_client_first_name))
+                .perform(scrollTo())
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.et_client_middle_name))
+                .perform(scrollTo())
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.et_client_last_name))
+                .perform(scrollTo())
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.et_client_mobile_no))
+                .perform(scrollTo())
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.et_client_external_id))
+                .perform(scrollTo())
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.sp_gender))
+                .perform(scrollTo())
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.tv_dateofbirth))
+                .perform(scrollTo())
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.sp_client_type))
+                .perform(scrollTo())
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.sp_client_classification))
+                .perform(scrollTo())
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.sp_offices))
+                .perform(scrollTo())
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.sp_staff))
+                .perform(scrollTo())
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.btn_submit))
+                .perform(scrollTo())
+                .check(matches(isDisplayed()));
+
+        onView(withId(R.id.cb_client_active_status))
+                .perform(scrollTo(), click())
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.tv_submission_date))
+                .perform(scrollTo())
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testViewsAreNotNull() {
+        assertNotNull(onView(withId(R.id.et_client_first_name)));
+        assertNotNull(onView(withId(R.id.et_client_middle_name)));
+        assertNotNull(onView(withId(R.id.et_client_last_name)));
+        assertNotNull(onView(withId(R.id.et_client_mobile_no)));
+        assertNotNull(onView(withId(R.id.et_client_external_id)));
+        assertNotNull(onView(withId(R.id.sp_gender)));
+        assertNotNull(onView(withId(R.id.tv_dateofbirth)));
+        assertNotNull(onView(withId(R.id.sp_client_type)));
+        assertNotNull(onView(withId(R.id.sp_client_classification)));
+        assertNotNull(onView(withId(R.id.sp_offices)));
+        assertNotNull(onView(withId(R.id.sp_staff)));
+        assertNotNull(onView(withId(R.id.cb_client_active_status)));
+        assertNotNull(onView(withId(R.id.tv_submission_date)));
+        assertNotNull(onView(withId(R.id.btn_submit)));
+    }
+
+    @Test
+    public void testOpenCreateClient() throws InterruptedException {
+        CreateNewClientFragment createNewClientFragment
+                = (CreateNewClientFragment) activityTestRule.getActivity()
                 .getSupportFragmentManager().findFragmentById(R.id.container);
-        textViewDateOfBirth = (TextView) dashboardActivity.findViewById(R.id.tv_dateofbirth);
-        textViewSubmissionDate = (TextView) dashboardActivity.findViewById(R.id.tv_submission_date);
-        checkBoxActive = (CheckBox) dashboardActivity.findViewById(R.id.cb_client_active_status);
+
+        assertNotNull(createNewClientFragment);
+        assertTrue(createNewClientFragment.isAdded());
     }
 
-    @SmallTest
-    public void testViewsNotNull() {
-        assertNotNull(textViewDateOfBirth);
-        assertNotNull(textViewSubmissionDate);
-        assertNotNull(checkBoxActive);
+    @Test
+    public void testCreateClient() throws InterruptedException {
+
+        Thread.sleep(3000);
+
+        // First Name
+        onView(withId(R.id.et_client_first_name))
+                .perform(scrollTo())
+                .perform(typeText("myFirstName"), closeSoftKeyboard());
+
+        // Middle Name
+        onView(withId(R.id.et_client_middle_name))
+                .perform(scrollTo())
+                .perform(typeText("myMiddleName"), closeSoftKeyboard());
+
+        // Last Name
+        onView(withId(R.id.et_client_last_name))
+                .perform(scrollTo())
+                .perform(typeText("myLastName"), closeSoftKeyboard());
+
+        // Mobile Number
+        onView(withId(R.id.et_client_mobile_no))
+                .perform(scrollTo())
+                .perform(typeText("987654321"), closeSoftKeyboard());
+
+        // External Id
+        onView(withId(R.id.et_client_external_id))
+                .perform(scrollTo())
+                .perform(typeText("123"), closeSoftKeyboard());
+
+        // Gender
+        Spinner spinnerGender = (Spinner) activityTestRule
+                .getActivity().findViewById(R.id.sp_gender);
+        String selectedGender = (String) spinnerGender.getAdapter().getItem(2);
+        onView(withId(R.id.sp_gender))
+                .perform(scrollTo(), click());
+        onView(withText(selectedGender))
+                .check(matches(isDisplayed()))
+                .perform(click())
+                .check(matches(isDisplayed()));
+
+        // Client
+        Spinner spinnerClient = (Spinner) activityTestRule
+                .getActivity().findViewById(R.id.sp_client_type);
+        String selectedClient = (String) spinnerClient.getAdapter().getItem(2);
+        onView(withId(R.id.sp_client_type))
+                .perform(scrollTo(), click());
+        onView(withText(selectedClient))
+                .check(matches(isDisplayed()))
+                .perform(click())
+                .check(matches(isDisplayed()));
+
+        // Client Classification
+        Spinner spinnerClientClassification = (Spinner) activityTestRule
+                .getActivity().findViewById(R.id.sp_client_classification);
+        String selectedClientClassification = (String) spinnerClientClassification
+                .getAdapter().getItem(1);
+        onView(withId(R.id.sp_client_classification))
+                .perform(scrollTo(), click());
+        onView(withText(selectedClientClassification))
+                .check(matches(isDisplayed()))
+                .perform(click())
+                .check(matches(isDisplayed()));
+
+        // Office
+        Spinner spinnerOffice = (Spinner) activityTestRule
+                .getActivity().findViewById(R.id.sp_offices);
+        String selectedOffice = (String) spinnerOffice.getAdapter().getItem(0);
+        onView(withId(R.id.sp_offices))
+                .perform(scrollTo(), click());
+        onView(withText(selectedOffice))
+                .check(matches(isDisplayed()))
+                .perform(click())
+                .check(matches(isDisplayed()));
+
+        // Staff
+        Spinner spinnerStaff = (Spinner) activityTestRule
+                .getActivity().findViewById(R.id.sp_staff);
+        String selectedStaff = (String) spinnerStaff.getAdapter().getItem(0);
+        onView(withId(R.id.sp_staff))
+                .perform(scrollTo(), click());
+        onView(withText(selectedStaff))
+                .check(matches(isDisplayed()))
+                .perform(click())
+                .check(matches(isDisplayed()));
+
+        // Activation Date
+        onView(withId(R.id.cb_client_active_status))
+                .perform(scrollTo(), click());
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        String currentDate = formatter.format(new Date());
+        onView(withId(R.id.tv_submission_date))
+                .check(matches(withText(currentDate)));
+
+        // Submit
+        onView(withId(R.id.btn_submit))
+                .perform(scrollTo(), click());
     }
 
-    /**
-     * - change birth date, check its text view is update
-     * - change submission date, check its text view is updated and birth date isn't
-     * - change birth date again, checks its text view is update, submission date isn't
-     */
-    @MediumTest
-    public void testChangeDates() throws InterruptedException {
-
-        // change birth date
-        String birthDate = "01-01-1990";
-        changeBirthDate(birthDate);
-        assertEquals(textViewDateOfBirth.getText().toString(), birthDate);
-
-        // check the active checkbox
-        dashboardActivity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                checkBoxActive.setChecked(true);
-            }
-        });
-        Thread.sleep(1000);
-        assertEquals(View.VISIBLE, textViewSubmissionDate.getVisibility());
-
-        // change submission date
-        final String submissionDate = "02-02-2000";
-        changeSubmissionDate(submissionDate);
-        assertEquals(textViewSubmissionDate.getText().toString(), submissionDate);
-        assertEquals(textViewDateOfBirth.getText().toString(), birthDate);
-
-        // change date of birth again
-        birthDate = "03-03-1989";
-        changeBirthDate(birthDate);
-        assertEquals(textViewSubmissionDate.getText().toString(), submissionDate);
-        assertEquals(textViewDateOfBirth.getText().toString(), birthDate);
+    private void openCreateClient() throws InterruptedException {
+        getInstrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_MENU);
+        onView(withText("Create Client"))
+                .perform(click());
     }
-
-    private void changeBirthDate(final String dateOfBirth) throws InterruptedException {
-
-        // open the date picker
-        onView(withId(R.id.tv_dateofbirth)).perform(scrollTo());
-        onView(withId(R.id.tv_dateofbirth)).perform(click());
-        Thread.sleep(1000);
-        assertTrue(createNewClientFragment.datePickerDateOfBirth.isAdded());
-
-        // update date and close date picker
-        dashboardActivity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                createNewClientFragment.onDatePicked(dateOfBirth);
-            }
-        });
-        createNewClientFragment.datePickerDateOfBirth.dismiss();
-        Thread.sleep(1000);
-    }
-
-    private void changeSubmissionDate(final String submissionDate) throws InterruptedException {
-        // open the date picker
-        onView(withId(R.id.tv_submission_date)).perform(scrollTo());
-        onView(withId(R.id.tv_submission_date)).perform(click());
-        Thread.sleep(1000);
-        assertTrue(createNewClientFragment.datePickerSubmissionDate.isAdded());
-
-        // update date and close date picker
-        dashboardActivity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                createNewClientFragment.onDatePicked(submissionDate);
-            }
-        });
-        createNewClientFragment.datePickerSubmissionDate.dismiss();
-        Thread.sleep(1000);
-    }
-
-
 }
