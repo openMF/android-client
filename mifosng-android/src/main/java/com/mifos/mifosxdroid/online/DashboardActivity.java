77dc651b5e84571f09be9cae11e38223150871f8
Danish Jamal <danishjamal.104@gmail.com>
2020-12-01 14:39:14 +0530

    NetworkChangeReciver implemented. And added auto toggle acting in onNewIntent in Dashboardactivity

--- mifosng-android/src/main/java/com/mifos/mifosxdroid/online/DashboardActivity.java
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/online/DashboardActivity.java
@@ -5,8 +5,11 @@
 
 package com.mifos.mifosxdroid.online;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
-
+import android.widget.Toast;
 import com.mifos.mifosxdroid.R;
 import com.mifos.mifosxdroid.core.MifosBaseActivity;
 import com.mifos.mifosxdroid.offlinejobs.OfflineSyncCenter;
@@ -15,6 +18,10 @@ import com.mifos.mifosxdroid.offlinejobs.OfflineSyncGroup;
 import com.mifos.mifosxdroid.offlinejobs.OfflineSyncLoanRepayment;
 import com.mifos.mifosxdroid.offlinejobs.OfflineSyncSavingsAccount;
 import com.mifos.mifosxdroid.online.search.SearchFragment;
+import com.mifos.mifosxdroid.receivers.NetworkChangeReceiver;
+import com.mifos.utils.Constants;
+import com.mifos.utils.PrefManager;
+
 
 import butterknife.ButterKnife;
 
@@ -22,6 +29,8 @@ import butterknife.ButterKnife;
  * Created by shashankpriyadarshi on 19/06/20.
  */
 public class DashboardActivity extends MifosBaseActivity {
+
+    NetworkChangeReceiver receiver;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,6 +40,23 @@ public class DashboardActivity extends MifosBaseActivity {
         ButterKnife.bind(this);
         //runJobs();
         replaceFragment(new SearchFragment(), false, R.id.container);
+
+        receiver = new NetworkChangeReceiver();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        int state = intent.getExtras().getInt(getString(R.string.network_state));
+        if (state != PrefManager.getUserStatus()) {
+            String info = "Auto switch to " +
+                    (state == Constants.USER_ONLINE ? "Online" : "offline") +
+                    " mode.";
+            Toast.makeText(this, info, Toast.LENGTH_LONG).show();
+        }
     }
 
     private void runJobs() {
