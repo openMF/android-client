b51c5cf025c0631d7aace5e2f35c734bbed92e41
PatelVatsalB21 <vatsal9974702102@gmail.com>
2020-12-06 12:18:58 +0530

    Changed sync switch to image button

--- mifosng-android/src/main/java/com/mifos/mifosxdroid/SettingsFragment.java
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/SettingsFragment.java
@@ -2,12 +2,19 @@ package com.mifos.mifosxdroid;
 
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.preference.SwitchPreference;;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.mifos.mifosxdroid.dialogfragments.syncsurveysdialog.SyncSurveysDialogFragment;
 import com.mifos.utils.FragmentConstants;
+import com.mifos.utils.Network;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by mayankjindal on 22/07/17.
@@ -15,7 +22,10 @@ import com.mifos.utils.FragmentConstants;
 
 public class SettingsFragment extends PreferenceFragment {
 
-    SwitchPreference mEnableSyncSurvey;
+    @BindView(R.id.settings_fragment_sync_button)
+    ImageButton syncButton;
+
+    private View rootView;
 
     public static SettingsFragment newInstance() {
         SettingsFragment fragment = new SettingsFragment();
@@ -27,25 +37,31 @@ public class SettingsFragment extends PreferenceFragment {
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
-        mEnableSyncSurvey = (SwitchPreference)
-                findPreference(getResources().getString(R.string.sync_survey));
-        mEnableSyncSurvey.setOnPreferenceChangeListener(
-                new Preference.OnPreferenceChangeListener() {
-                @Override
-                public boolean onPreferenceChange(Preference preference, Object newValue) {
-                    if (((Boolean) newValue)) {
-                        SyncSurveysDialogFragment syncSurveysDialogFragment =
-                                SyncSurveysDialogFragment.newInstance();
-                        FragmentTransaction fragmentTransaction =
-                                getFragmentManager().beginTransaction();
-                            fragmentTransaction.addToBackStack(FragmentConstants.FRAG_SURVEYS_SYNC);
-                        syncSurveysDialogFragment.setCancelable(false);
-                        syncSurveysDialogFragment.show(fragmentTransaction,
-                                getResources().getString(R.string.sync_clients));
-                    }
-                    return true;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
+            Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+        ButterKnife.bind(this, rootView);
+        syncButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Network.isOnline(container.getContext())) {
+                    SyncSurveysDialogFragment syncSurveysDialogFragment =
+                            SyncSurveysDialogFragment.newInstance();
+                    FragmentTransaction fragmentTransaction =
+                            getFragmentManager().beginTransaction();
+                    fragmentTransaction.addToBackStack(FragmentConstants.FRAG_SURVEYS_SYNC);
+                    syncSurveysDialogFragment.setCancelable(false);
+                    syncSurveysDialogFragment.show(fragmentTransaction,
+                            getResources().getString(R.string.sync_clients));
+                } else {
+                    Toast.makeText(container.getContext(),
+                          "Please Check Your Internet Connection", Toast.LENGTH_SHORT).show();
                 }
-            });
+            }
+        });
+        return rootView;
     }
 }
