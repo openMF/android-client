8ef6bb292d95dc80fd674d3b48c9b5e9b5cda739
Olawale Jamiu <61050994+Gprocessor@users.noreply.github.com>
2023-06-18 08:09:45 -0700

    Update CreateNewCenterFragment date

--- mifosng-android/src/main/java/com/mifos/mifosxdroid/online/createnewcenter/CreateNewCenterFragment.kt
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/online/createnewcenter/CreateNewCenterFragment.kt
@@ -102,7 +102,7 @@ class CreateNewCenterFragment : MifosBaseFragment(), OnDatePickListener, CreateN
             centerPayload.isActive = cb_centerActiveStatus!!.isChecked
             centerPayload.activationDate = activationdateString
             centerPayload.officeId = officeId
-            centerPayload.dateFormat = "dd MMMM yyyy"
+            centerPayload.dateFormat = "dd MMM yyyy"
             centerPayload.locale = "en"
             initiateCenterCreation(centerPayload)
         }
