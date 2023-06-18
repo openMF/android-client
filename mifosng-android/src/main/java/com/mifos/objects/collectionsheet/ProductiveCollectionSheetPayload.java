ac7e85fdfedcd52c160eb96b786cc846fe41a120
Olawale Jamiu <61050994+Gprocessor@users.noreply.github.com>
2023-06-18 17:14:50 +0100

    Update

--- mifosng-android/src/main/java/com/mifos/objects/collectionsheet/ProductiveCollectionSheetPayload.java
+++ mifosng-android/src/main/java/com/mifos/objects/collectionsheet/ProductiveCollectionSheetPayload.java
@@ -18,7 +18,7 @@ public class ProductiveCollectionSheetPayload implements Parcelable {
 
     private int calendarId;
 
-    private String dateFormat = "dd MMM yyyy";
+    private String dateFormat = "dd MMMM YYYY";
 
     private String locale = "en";
 
