e794555298b23a925bb9305068a5053dd27a33f0
Olawale Jamiu <61050994+Gprocessor@users.noreply.github.com>
2023-06-18 08:18:35 -0700

    Update ActivatePayload.java

--- mifosng-android/src/main/java/com/mifos/objects/client/ActivatePayload.java
+++ mifosng-android/src/main/java/com/mifos/objects/client/ActivatePayload.java
@@ -15,7 +15,7 @@ public class ActivatePayload implements Parcelable {
     String activationDate;
 
     @SerializedName("dateFormat")
-    String dateFormat = "dd MMMM YYYY";
+    String dateFormat = "dd MMM YYYY";
 
     @SerializedName("locale")
     String locale = "en";
