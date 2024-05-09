f14af1b94495f33f3ae27d1ac650cc306adc1be8
Aditya Gupta <ashishgupta9936ad@gmail.com>
2024-05-09 12:29:38 +0530

    fix: code clean up

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,38 +1,13 @@
 plugins {
     alias(libs.plugins.mifos.android.library.compose)
-//    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.mifos.android.library)
 }
 
 android {
     namespace = "com.mifos.core.designsystem"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.material)
-    testImplementation(libs.junit4)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.test.espresso.core)
 
     api(libs.androidx.compose.foundation)
     api(libs.androidx.compose.foundation.layout)
