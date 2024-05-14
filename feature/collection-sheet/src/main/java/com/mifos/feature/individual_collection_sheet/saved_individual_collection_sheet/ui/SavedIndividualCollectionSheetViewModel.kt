d4b90bccabdf9eaa53a21a8bdc39177c4dedd105
Aditya Gupta <94394661+Aditya-gupta99@users.noreply.github.com>
2024-05-14 21:50:39 +0530

    refactor: refactor SavedIndividual Collection Sheet screen to jetpack compose with multi module (#2097)
    * refactor: refactor savedindividual collection sheet to compose with multi module
    
    * fix: clean up logs
    
    * fix: log clean up
    
    * refactor: rename module

---
+++ feature/collection-sheet/src/main/java/com/mifos/feature/individual_collection_sheet/saved_individual_collection_sheet/ui/SavedIndividualCollectionSheetViewModel.kt
@@ -0,0 +1,12 @@
+package com.mifos.feature.individual_collection_sheet.saved_individual_collection_sheet.ui
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SavedIndividualCollectionSheetViewModel @Inject constructor() : ViewModel() {
+
+    // Saved Individual Collection Sheet need to be integrated with API.
+
+}
