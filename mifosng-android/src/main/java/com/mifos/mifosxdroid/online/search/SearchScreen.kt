083c62f3aee4c5369fbd11658e202749967cc34d
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2024-05-20 18:58:27 +0530

    Fix the TextField moved to below on click issue

--- mifosng-android/src/main/java/com/mifos/mifosxdroid/online/search/SearchScreen.kt
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/online/search/SearchScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -111,7 +112,8 @@ fun SearchScreen(
                             )
                         }
                     }
-                }
+                },
+                windowInsets = WindowInsets(0, 0, 0, 0)
             )
         },
         snackbarHost = {
