1d06423957550b502064be3e1af3ab5bab542712
Pronay sarker <pronaycoding@gmail.com>
2024-06-28 04:37:05 +0600

    Migrate Data Table Fragment to compose
    migrate dataTable to compose
    
    topbar fix
    
    removed extra file
    
    fix topbar
    
    made required changes

--- mifosng-android/src/main/java/com/mifos/mifosxdroid/online/datatable/DataTableFragment.kt
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/online/datatable/DataTableFragment.kt
@@ -4,52 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout.OnRefreshListener
 import com.mifos.core.objects.noncore.DataTable
-import com.mifos.mifosxdroid.R
-import com.mifos.mifosxdroid.adapters.DataTableAdapter
 import com.mifos.mifosxdroid.core.MifosBaseFragment
-import com.mifos.mifosxdroid.core.util.Toaster
-import com.mifos.mifosxdroid.databinding.FragmentDatatablesBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * Created by Rajan Maurya on 12/02/17.
  */
 @AndroidEntryPoint
-class DataTableFragment : MifosBaseFragment(), OnRefreshListener {
+class DataTableFragment : MifosBaseFragment() {
 
-    private lateinit var binding: FragmentDatatablesBinding
     private val arg: DataTableFragmentArgs by navArgs()
 
-    private lateinit var viewModel: DataTableViewModel
-
-
-    private val dataTableAdapter by lazy {
-        DataTableAdapter(
-            onDateTableClick = { dataTable ->
-                val action =
-                    DataTableFragmentDirections.actionDataTableFragmentToDataTableDataFragment(
-                        dataTable,
-                        entityId
-                    )
-                findNavController().navigate(action)
-            }
-        )
-    }
     private var tableName: String? = null
     private var entityId = 0
-    private var dataTables: List<DataTable>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         tableName = arg.tableName
         entityId = arg.entityId
-        dataTables = ArrayList()
     }
 
     override fun onCreateView(
@@ -57,87 +34,37 @@ class DataTableFragment : MifosBaseFragment(), OnRefreshListener {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentDatatablesBinding.inflate(inflater, container, false)
-        viewModel = ViewModelProvider(this)[DataTableViewModel::class.java]
-        showUserInterface()
-        viewModel.loadDataTable(tableName)
-
-        viewModel.dataTableUiState.observe(viewLifecycleOwner) {
-            when (it) {
-                is DataTableUiState.ShowDataTables -> {
-                    showProgressbar(false)
-                    showDataTables(it.dataTables)
-                }
-
-                is DataTableUiState.ShowEmptyDataTables -> {
-                    showProgressbar(false)
-                    showEmptyDataTables()
-                }
-
-                is DataTableUiState.ShowError -> {
-                    showProgressbar(false)
-                    showError(it.message)
-                }
-
-                is DataTableUiState.ShowProgressbar -> showProgressbar(true)
-                is DataTableUiState.ShowResetVisibility -> {
-                    showProgressbar(false)
-                    showResetVisibility()
-                }
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                DataTableScreen(
+                    tableName = tableName,
+                    navigateBack = {
+                        findNavController().popBackStack()                    },
+                    onClick = {
+                        navigateToDatatableData(dataTable = it)
+                    }
+                )
             }
         }
-
-        return binding.root
     }
 
-    override fun onRefresh() {
-        viewModel.loadDataTable(tableName)
+    private fun navigateToDatatableData(dataTable: DataTable) {
+        val action =
+            DataTableFragmentDirections.actionDataTableFragmentToDataTableDataFragment(
+                dataTable,
+                entityId
+            )
+        findNavController().navigate(action)
     }
 
-    private fun showUserInterface() {
-        setToolbarTitle(resources.getString(R.string.datatables))
-        val mLayoutManager = LinearLayoutManager(activity)
-        mLayoutManager.orientation = LinearLayoutManager.VERTICAL
-        binding.rvDataTable.layoutManager = mLayoutManager
-        binding.rvDataTable.setHasFixedSize(true)
-        binding.rvDataTable.adapter = dataTableAdapter
-        binding.swipeContainer.setColorSchemeColors(
-            *activity
-                ?.resources!!.getIntArray(R.array.swipeRefreshColors)
-        )
-        binding.swipeContainer.setOnRefreshListener(this)
+    override fun onResume() {
+        super.onResume()
+        toolbar?.visibility = View.GONE
     }
 
-    private fun showDataTables(dataTables: List<DataTable>?) {
-        this.dataTables = dataTables
-        dataTableAdapter.dataTables = dataTables ?: emptyList()
-    }
-
-    private fun showEmptyDataTables() {
-        binding.llError.visibility = View.VISIBLE
-        binding.rvDataTable.visibility = View.GONE
-        binding.tvError.setText(R.string.empty_data_table)
-    }
-
-    private fun showResetVisibility() {
-        binding.llError.visibility = View.GONE
-        binding.rvDataTable.visibility = View.VISIBLE
-    }
-
-    private fun showError(message: Int) {
-        Toaster.show(binding.root, message)
-        binding.llError.visibility = View.VISIBLE
-        binding.rvDataTable.visibility = View.GONE
-        binding.tvError.text = getString(R.string.failed_to_fetch_datatable)
-    }
-
-    private fun showProgressbar(show: Boolean) {
-        if (show && dataTableAdapter.itemCount == 0) {
-            binding.progressbarDataTable.visibility = View.VISIBLE
-            binding.swipeContainer.isRefreshing = false
-        } else {
-            binding.progressbarDataTable.visibility = View.GONE
-            binding.swipeContainer.isRefreshing = show
-        }
+    override fun onStop() {
+        super.onStop()
+        toolbar?.visibility = View.VISIBLE
     }
 }
--- mifosng-android/src/main/java/com/mifos/mifosxdroid/online/datatable/DataTableViewModel.kt
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/online/datatable/DataTableViewModel.kt
@@ -3,9 +3,16 @@ package com.mifos.mifosxdroid.online.datatable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.mifos.core.objects.noncore.DataTable
+import com.mifos.feature.note.NoteUiState
 import com.mifos.mifosxdroid.R
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import okhttp3.internal.wait
 import rx.Subscriber
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
@@ -18,19 +25,27 @@ import javax.inject.Inject
 class DataTableViewModel @Inject constructor(private val repository: DataTableRepository) :
     ViewModel() {
 
-    private val _dataTableUiState = MutableLiveData<DataTableUiState>()
+    private val _dataTableUiState =
+        MutableStateFlow<DataTableUiState>(DataTableUiState.ShowProgressbar)
+    val dataTableUiState: StateFlow<DataTableUiState> get() = _dataTableUiState
 
-    val dataTableUiState: LiveData<DataTableUiState>
-        get() = _dataTableUiState
+    private val _isRefreshing = MutableStateFlow(false)
+    val isRefreshing: StateFlow<Boolean> get() = _isRefreshing.asStateFlow()
+
+    fun refresh(tableName: String?) {
+        viewModelScope.launch {
+            _isRefreshing.emit(true)
+            loadDataTable(tableName)
+            _isRefreshing.value = false
+        }
+    }
 
     fun loadDataTable(tableName: String?) {
         _dataTableUiState.value = DataTableUiState.ShowProgressbar
-        _dataTableUiState.value = DataTableUiState.ShowResetVisibility
-        repository.getDataTable(tableName)
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeOn(Schedulers.io())
-            .subscribe(object : Subscriber<List<DataTable>>() {
+        repository.getDataTable(tableName).observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io()).subscribe(object : Subscriber<List<DataTable>>() {
                 override fun onCompleted() {}
+
                 override fun onError(e: Throwable) {
                     _dataTableUiState.value =
                         DataTableUiState.ShowError(R.string.failed_to_fetch_datatable)
@@ -47,3 +62,4 @@ class DataTableViewModel @Inject constructor(private val repository: DataTableRe
     }
 
 }
+
