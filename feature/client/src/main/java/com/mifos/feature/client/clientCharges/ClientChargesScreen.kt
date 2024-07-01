51bbd5a1fd70f9fc0eea792fc9da5ed983ffe7bf
Aditya Gupta <94394661+Aditya-gupta99@users.noreply.github.com>
2024-07-01 21:32:21 +0530

    refactor: refactor client charge fragment to compose (#2120)

---
+++ feature/client/src/main/java/com/mifos/feature/client/clientCharges/ClientChargesScreen.kt
@@ -0,0 +1,291 @@
+@file:OptIn(ExperimentalMaterialApi::class)
+
+package com.mifos.feature.client.clientCharges
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.paging.LoadState
+import androidx.paging.PagingData
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.mifos.core.designsystem.component.MifosCircularProgress
+import com.mifos.core.designsystem.component.MifosPagingAppendProgress
+import com.mifos.core.designsystem.component.MifosScaffold
+import com.mifos.core.designsystem.component.MifosSweetError
+import com.mifos.core.designsystem.icon.MifosIcons
+import com.mifos.core.designsystem.theme.Black
+import com.mifos.core.designsystem.theme.BlueSecondary
+import com.mifos.core.designsystem.theme.DarkGray
+import com.mifos.core.objects.client.Charges
+import com.mifos.feature.client.R
+import kotlinx.coroutines.flow.flowOf
+
+@Composable
+fun ClientChargesScreen(
+    clientId: Int,
+    onBackPressed: () -> Unit
+) {
+
+    val viewModel: ClientChargesViewModel = hiltViewModel()
+    val clientChargeUiState by viewModel.clientChargesUiState.collectAsStateWithLifecycle()
+    val refreshState by viewModel.isRefreshing.collectAsStateWithLifecycle()
+
+    LaunchedEffect(Unit) {
+        viewModel.loadCharges(clientId)
+    }
+
+    ClientChargesScreen(
+        state = clientChargeUiState,
+        onBackPressed = onBackPressed,
+        onRetry = { viewModel.loadCharges(clientId) },
+        onRefresh = { viewModel.refreshCenterList(clientId) },
+        refreshState = refreshState,
+        addCharges = {
+            // TODO Implement Dialog to add charges
+        }
+    )
+
+}
+
+@Composable
+fun ClientChargesScreen(
+    state: ClientChargeUiState,
+    onBackPressed: () -> Unit,
+    onRetry: () -> Unit,
+    onRefresh: () -> Unit,
+    refreshState: Boolean,
+    addCharges: () -> Unit
+) {
+
+    val snackbarHostState = remember { SnackbarHostState() }
+    val pullRefreshState = rememberPullRefreshState(
+        refreshing = refreshState,
+        onRefresh = onRefresh
+    )
+
+    MifosScaffold(
+        icon = MifosIcons.arrowBack,
+        title = stringResource(id = R.string.feature_client_charges),
+        onBackPressed = onBackPressed,
+        actions = {
+            IconButton(onClick = { addCharges() }) {
+                Icon(imageVector = MifosIcons.Add, contentDescription = null)
+            }
+        },
+        snackbarHostState = snackbarHostState
+    ) { paddingValues ->
+        Column(modifier = Modifier.padding(paddingValues)) {
+            Box(modifier = Modifier.pullRefresh(pullRefreshState)) {
+                when (state) {
+                    is ClientChargeUiState.ChargesList -> ClientChargeContent(
+                        chargesPage = state.chargesPage.collectAsLazyPagingItems(),
+                        onRetry = onRetry
+                    )
+
+                    is ClientChargeUiState.Error -> MifosSweetError(message = stringResource(id = state.message)) {
+                        onRetry()
+                    }
+
+                    is ClientChargeUiState.Loading -> MifosCircularProgress()
+                }
+                PullRefreshIndicator(
+                    refreshing = refreshState,
+                    state = pullRefreshState,
+                    modifier = Modifier.align(Alignment.TopCenter)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ClientChargeContent(
+    chargesPage: LazyPagingItems<Charges>,
+    onRetry: () -> Unit
+) {
+
+    when (chargesPage.loadState.refresh) {
+        is LoadState.Error -> {
+            MifosSweetError(message = stringResource(id = R.string.feature_client_failed_to_load_client_charges)) {
+                onRetry()
+            }
+        }
+
+        is LoadState.Loading -> MifosCircularProgress()
+
+        is LoadState.NotLoading -> Unit
+    }
+
+
+    LazyColumn {
+        items(chargesPage.itemCount) { index ->
+            chargesPage[index]?.let { ChargesItems(it) }
+        }
+
+        when (chargesPage.loadState.append) {
+            is LoadState.Error -> {
+
+            }
+
+            is LoadState.Loading -> {
+                item {
+                    MifosPagingAppendProgress()
+                }
+            }
+
+            is LoadState.NotLoading -> Unit
+        }
+        when (chargesPage.loadState.append.endOfPaginationReached) {
+            true -> {
+                item {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(6.dp),
+                        text = stringResource(id = R.string.feature_client_no_more_charges_available),
+                        style = TextStyle(
+                            fontSize = 14.sp
+                        ),
+                        color = DarkGray,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+
+            false -> Unit
+        }
+    }
+}
+
+
+@Composable
+fun ChargesItems(charges: Charges) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        shape = RoundedCornerShape(0.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = BlueSecondary
+        )
+    ) {
+        Spacer(modifier = Modifier.height(8.dp))
+        MifosCenterDetailsText(
+            stringResource(id = R.string.feature_client_client_id),
+            charges.chargeId.toString()
+        )
+        MifosCenterDetailsText(
+            stringResource(id = R.string.feature_client_charge_name),
+            charges.name ?: ""
+        )
+        MifosCenterDetailsText(
+            stringResource(id = R.string.feature_client_charge_amount),
+            charges.amount.toString()
+        )
+        MifosCenterDetailsText(
+            stringResource(id = R.string.feature_client_due_date),
+            charges.formattedDueDate
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+    }
+}
+
+@Composable
+fun MifosCenterDetailsText(field: String, value: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp),
+            text = field,
+            style = TextStyle(
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal,
+                fontStyle = FontStyle.Normal
+            ),
+            color = Black,
+            textAlign = TextAlign.Start
+        )
+        Text(
+            modifier = Modifier.weight(1f),
+            text = value,
+            style = TextStyle(
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal,
+                fontStyle = FontStyle.Normal
+            ),
+            color = DarkGray,
+            textAlign = TextAlign.Start
+        )
+    }
+}
+
+class ClientChargesScreenUiStateProvider : PreviewParameterProvider<ClientChargeUiState> {
+
+    override val values: Sequence<ClientChargeUiState>
+        get() = sequenceOf(
+            ClientChargeUiState.Loading,
+            ClientChargeUiState.Error(R.string.feature_client_failed_to_load_client_charges),
+            ClientChargeUiState.ChargesList(flowOf(PagingData.from(sampleClientCharge)))
+        )
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ClientChargesScreenPreview(
+    @PreviewParameter(ClientChargesScreenUiStateProvider::class) state: ClientChargeUiState
+) {
+    ClientChargesScreen(
+        state = state,
+        onBackPressed = {},
+        onRetry = {},
+        onRefresh = {},
+        refreshState = false,
+        addCharges = {}
+    )
+}
+
+val sampleClientCharge = List(10) {
+    Charges(name = "charge $it", amount = it.toDouble())
+}
--- mifosng-android/src/main/java/com/mifos/mifosxdroid/online/clientcharge/ClientChargeFragment.kt
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/online/clientcharge/ClientChargeFragment.kt
@@ -6,49 +6,29 @@ package com.mifos.mifosxdroid.online.clientcharge
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.res.ResourcesCompat
-import androidx.lifecycle.ViewModelProvider
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.mifos.core.objects.client.Charges
-import com.mifos.core.objects.client.Page
-import com.mifos.mifosxdroid.R
-import com.mifos.mifosxdroid.adapters.ChargeNameListAdapter
-import com.mifos.mifosxdroid.core.EndlessRecyclerOnScrollListener
+import com.mifos.feature.client.clientCharges.ClientChargesScreen
 import com.mifos.mifosxdroid.core.MifosBaseFragment
-import com.mifos.mifosxdroid.core.util.Toaster
-import com.mifos.mifosxdroid.databinding.FragmentChargeListBinding
-import com.mifos.mifosxdroid.dialogfragments.chargedialog.ChargeDialogFragment
-import com.mifos.mifosxdroid.dialogfragments.chargedialog.OnChargeCreateListener
-import com.mifos.utils.FragmentConstants
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * Created by nellyk on 1/22/2016.
  */
 @AndroidEntryPoint
-class ClientChargeFragment : MifosBaseFragment(), OnChargeCreateListener {
+class ClientChargeFragment : MifosBaseFragment() {
 
-
-    private lateinit var binding: FragmentChargeListBinding
     private val arg: ClientChargeFragmentArgs by navArgs()
-
-    private lateinit var chargesList: List<Charges>
-
-    private lateinit var viewModel: ClientChargeViewModel
-
-    private var mChargesNameListAdapter: ChargeNameListAdapter? = null
     private var clientId = 0
-    private var mApiRestCounter = 0
-    private val limit = 10
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         clientId = arg.clientId
-        setChargesList(arg.chargesList.toMutableList())
     }
 
     override fun onCreateView(
@@ -56,197 +36,36 @@ class ClientChargeFragment : MifosBaseFragment(), OnChargeCreateListener {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentChargeListBinding.inflate(inflater, container, false)
-        setHasOptionsMenu(true)
-        viewModel = ViewModelProvider(this)[ClientChargeViewModel::class.java]
-        val layoutManager = LinearLayoutManager(activity)
-        layoutManager.orientation = LinearLayoutManager.VERTICAL
-        binding.rvCharge.layoutManager = layoutManager
-        binding.rvCharge.setHasFixedSize(true)
-        setToolbarTitle(getString(R.string.charges))
-        mApiRestCounter = 1
-        viewModel.loadCharges(clientId, 0, limit)
-        /**
-         * Setting mApiRestCounter to 1 and send Refresh Request to Server
-         */
-        binding.swipeContainer.setColorSchemeResources(
-            R.color.blue_light,
-            R.color.green_light,
-            R.color.orange_light,
-            R.color.red_light
-        )
-        binding.swipeContainer.setOnRefreshListener {
-            mApiRestCounter = 1
-            viewModel.loadCharges(clientId, 0, limit)
-            if (binding.swipeContainer.isRefreshing) binding.swipeContainer.isRefreshing = false
-        }
-        loadMore(layoutManager)
-
-        viewModel.clientChargeUiState.observe(viewLifecycleOwner) {
-            when (it) {
-                is ClientChargeUiState.ShowChargesList -> {
-                    showProgressbar(false)
-                    showChargesList(it.chargesPage)
-                }
-
-                is ClientChargeUiState.ShowEmptyCharges -> {
-                    showProgressbar(false)
-                    showEmptyCharges()
-                }
-
-                is ClientChargeUiState.ShowFetchingErrorCharges -> {
-                    showProgressbar(false)
-                    showFetchingErrorCharges(it.message)
-                }
-
-                is ClientChargeUiState.ShowProgressbar -> showProgressbar(true)
+        return ComposeView(requireActivity()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                ClientChargesScreen(
+                    clientId = clientId,
+                    onBackPressed = {
+                        findNavController().popBackStack()
+                    }
+                )
             }
         }
-
-        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.noChargesIcon.setOnClickListener {
-            reloadOnError()
-        }
+    override fun onResume() {
+        super.onResume()
+        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
     }
 
-    /**
-     * This is the LoadMore of the RecyclerView. It called When Last Element of RecyclerView
-     * is shown on the Screen.
-     * Increase the mApiRestCounter by 1 and Send Api Request to Server with Paged(True)
-     * and offset(mCenterList.size()) and limit(100).
-     */
-    private fun loadMore(layoutManager: LinearLayoutManager) {
-        binding.rvCharge.addOnScrollListener(object :
-            EndlessRecyclerOnScrollListener(layoutManager) {
-            override fun onLoadMore(current_page: Int) {
-                mApiRestCounter += 1
-                viewModel.loadCharges(clientId, chargesList.size, limit)
-            }
-        })
+    override fun onStop() {
+        super.onStop()
+        (requireActivity() as AppCompatActivity).supportActionBar?.show()
     }
 
-    /**
-     * Shows When mApiRestValue is 1 and Server Response is Null.
-     * Onclick Send Fresh Request for Center list.
-     */
-
-    private fun reloadOnError() {
-        binding.llError.visibility = View.GONE
-        viewModel.loadCharges(clientId, 0, limit)
+    fun addChargesDialog() {
+//        val chargeDialogFragment = ChargeDialogFragment.newInstance(clientId)
+//        chargeDialogFragment.setOnChargeCreatedListener(this)
+//        val fragmentTransaction = requireActivity().supportFragmentManager
+//            .beginTransaction()
+//        fragmentTransaction.addToBackStack(FragmentConstants.FRAG_CHARGE_LIST)
+//        chargeDialogFragment.show(fragmentTransaction, "Charge Dialog Fragment")
     }
-
-    fun setChargesList(chargesList: MutableList<Charges>) {
-        this.chargesList = chargesList as ArrayList<Charges>
-    }
-
-    private fun showChargesList(chargesPage: Page<Charges>) {
-        /**
-         * if mApiRestCounter is 1, So this is the first Api Request.
-         * else if mApiRestCounter is greater than 1, SO this is for loadmore request.
-         */
-        if (mApiRestCounter == 1) {
-            chargesList = chargesPage.pageItems
-            mChargesNameListAdapter = ChargeNameListAdapter(chargesList, clientId)
-            binding.rvCharge.adapter = mChargesNameListAdapter
-            binding.llError.visibility = View.GONE
-        } else {
-            chargesList.addAll()
-            mChargesNameListAdapter?.notifyDataSetChanged()
-
-            //checking the response size if size is zero then show toast No More
-            // Clients Available for fetch
-            if (chargesPage.pageItems.size == 0 &&
-                chargesPage.totalFilteredRecords == chargesList.size
-            ) Toaster.show(binding.root, getString(R.string.message_no_more_charge))
-        }
-    }
-
-    override fun onChargeCreatedSuccess(charge: Charges) {
-        chargesList.add()
-        Toaster.show(binding.root, getString(R.string.message_charge_created_success))
-        if (binding.llError.visibility == View.VISIBLE) {
-            binding.llError.visibility = View.GONE
-        }
-        //If the adapter has not been initialized, there were 0 charge items earlier. Initialize it.
-        if (mChargesNameListAdapter == null) {
-            mChargesNameListAdapter = ChargeNameListAdapter(chargesList, clientId)
-            binding.rvCharge.adapter = mChargesNameListAdapter
-        }
-        mChargesNameListAdapter?.notifyItemInserted(chargesList.size - 1)
-    }
-
-    override fun onChargeCreatedFailure(errorMessage: String) {
-        Toaster.show(binding.root, errorMessage)
-    }
-
-    private fun showEmptyCharges() {
-        if (mChargesNameListAdapter == null || mChargesNameListAdapter?.itemCount == 0) {
-            binding.llError.visibility = View.VISIBLE
-            binding.noChargesText.text =
-                resources.getString(R.string.message_no_charges_available)
-            binding.noChargesIcon.setImageResource(R.drawable.ic_assignment_turned_in_black_24dp)
-        }
-    }
-
-    private fun showFetchingErrorCharges(s: String) {
-        if (mApiRestCounter == 1) {
-            binding.llError.visibility = View.VISIBLE
-            binding.noChargesText.text = "$s\n Click to Refresh "
-        }
-        Toaster.show(binding.root, s)
-    }
-
-    private fun showProgressbar(b: Boolean) {
-        if (mApiRestCounter == 1) {
-            if (b) {
-                showMifosProgressBar()
-            } else {
-                hideMifosProgressBar()
-            }
-        } else {
-            binding.swipeContainer.isRefreshing = b
-        }
-    }
-
-    override fun onPrepareOptionsMenu(menu: Menu) {
-        menu.clear()
-        val menuItemAddNewDocument =
-            menu.add(Menu.NONE, MENU_ITEM_ADD_NEW_CHARGES, Menu.NONE, getString(R.string.add_new))
-        menuItemAddNewDocument.icon = ResourcesCompat.getDrawable(
-            resources,
-            R.drawable.ic_add_white_24dp, null
-        )
-        menuItemAddNewDocument.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM)
-        super.onPrepareOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-        if (id == MENU_ITEM_ADD_NEW_CHARGES) {
-            val chargeDialogFragment = ChargeDialogFragment.newInstance(clientId)
-            chargeDialogFragment.setOnChargeCreatedListener(this)
-            val fragmentTransaction = requireActivity().supportFragmentManager
-                .beginTransaction()
-            fragmentTransaction.addToBackStack(FragmentConstants.FRAG_CHARGE_LIST)
-            chargeDialogFragment.show(fragmentTransaction, "Charge Dialog Fragment")
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    companion object {
-        const val MENU_ITEM_ADD_NEW_CHARGES = 2000
-    }
-}
-
-private fun <E> List<E>.add() {
-
-}
-
-private fun <E> List<E>.addAll() {
 
 }
