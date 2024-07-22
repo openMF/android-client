62ed2a7723becb549a417b4d3afe5c05ab04077f
Avneet Singh <avneetmaankiya@gmail.com>
2024-07-22 05:39:11 +0530

    refactor: data table list migration

---
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/online/datatablelistfragment/DataTableListScreen.kt
@@ -0,0 +1,15 @@
+package com.mifos.mifosxdroid.online.datatablelistfragment
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun DataTableListScreen() {
+
+}
+
+@Preview
+@Composable
+fun DataTableListScreenPreview() {
+    DataTableListScreen()
+}
