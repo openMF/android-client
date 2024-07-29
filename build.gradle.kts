c9a171851d6549e33add6774c7e1aaf8e9c9ccee
Sk Niyaj Ali <niyaj639@gmail.com>
2024-07-29 20:08:13 +0530

    feat: configured detekt plugin
    - Applied common rules
    - Applied Twitter Compose Rules

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,39 @@ plugins {
     alias(libs.plugins.androidx.navigation) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.secrets) apply false
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.detekt.compiler)
+}
+
+val detektFormatting = libs.detekt.formatting
+val twitterComposeRules = libs.twitter.detekt.compose
+
+val reportMerge by tasks.registering(io.gitlab.arturbosch.detekt.report.ReportMergeTask::class) {
+    output.set(rootProject.layout.buildDirectory.file("reports/detekt/merge.html")) // or "reports/detekt/merge.sarif"
+}
+
+subprojects {
+    apply {
+        plugin("io.gitlab.arturbosch.detekt")
+    }
+
+    detekt {
+        config.from(rootProject.files("config/detekt/detekt.yml"))
+        reports.xml.required.set(true)
+    }
+
+    tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+        finalizedBy(reportMerge)
+    }
+
+    reportMerge {
+        input.from(tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().map {
+            it.htmlReportFile }
+        )
+    }
+
+    dependencies {
+        detektPlugins(detektFormatting)
+        detektPlugins(twitterComposeRules)
+    }
 }
