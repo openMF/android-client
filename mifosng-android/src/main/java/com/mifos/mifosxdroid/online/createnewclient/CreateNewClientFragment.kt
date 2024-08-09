a82f3c764a2f58a0c2720184b83e54bc831e8c28
Pronay sarker <pronaycoding@gmail.com>
2024-08-09 22:50:07 +0600

    MIFOSSAC-240 moved savings to savings module

--- mifosng-android/src/main/java/com/mifos/mifosxdroid/online/createnewclient/CreateNewClientFragment.kt
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/online/createnewclient/CreateNewClientFragment.kt
@@ -4,55 +4,21 @@
  */
 package com.mifos.mifosxdroid.online.createnewclient
 
-import android.Manifest
-import android.app.Activity
-import android.app.ProgressDialog
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.BitmapFactory
-import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
-import android.telephony.PhoneNumberUtils
-import android.text.TextUtils
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.appcompat.widget.PopupMenu
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.core.app.ActivityCompat
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.mifos.core.common.utils.Constants
 import com.mifos.core.objects.client.ClientPayload
 import com.mifos.core.objects.noncore.DataTable
-import com.mifos.core.objects.noncore.DataTablePayload
-import com.mifos.core.objects.organisation.Office
-import com.mifos.core.objects.organisation.Staff
-import com.mifos.core.objects.templates.clients.ClientsTemplate
-import com.mifos.exceptions.InvalidTextInputException
-import com.mifos.exceptions.RequiredFieldException
 import com.mifos.mifosxdroid.R
 import com.mifos.mifosxdroid.core.MifosBaseFragment
-import com.mifos.mifosxdroid.core.ProgressableFragment
-import com.mifos.mifosxdroid.core.util.Toaster
-import com.mifos.mifosxdroid.databinding.FragmentCreateNewClientBinding
-import com.mifos.mifosxdroid.online.createnewgroup.CreateNewGroupScreen
 import com.mifos.mifosxdroid.online.datatablelistfragment.DataTableListFragment
-import com.mifos.utils.DatePickerConstrainType
 import com.mifos.utils.FragmentConstants
-import com.mifos.utils.ValidationUtil
-import com.mifos.utils.getDatePickerDialog
-import com.mifos.utils.getTodayFormatted
 import dagger.hilt.android.AndroidEntryPoint
-import java.io.File
-import java.text.SimpleDateFormat
-import java.time.Instant
-import java.util.Locale
 
 @AndroidEntryPoint
 class CreateNewClientFragment : MifosBaseFragment() {
