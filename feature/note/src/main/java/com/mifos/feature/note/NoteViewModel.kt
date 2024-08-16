d10e9aca77fc3ee74515352ed2c83305d5b47137
Pronay sarker <pronaycoding@gmail.com>
2024-08-17 01:14:38 +0600

    MIFOSAC-257: implemented compose navigation in note module
    navigation
    
    MIFOSAC-257: implemented compose navigation

--- feature/note/src/main/java/com/mifos/feature/note/NoteViewModel.kt
+++ feature/note/src/main/java/com/mifos/feature/note/NoteViewModel.kt
@@ -1,27 +1,28 @@
 package com.mifos.feature.note
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import android.util.Log
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.mifos.core.data.repository.NoteRepository
+import com.mifos.core.common.utils.Constants
 import com.mifos.core.data.repository_imp.NoteRepositoryImp
-import com.mifos.core.objects.noncore.Note
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import rx.Subscriber
-import rx.android.schedulers.AndroidSchedulers
-import rx.schedulers.Schedulers
 import javax.inject.Inject
 
 @HiltViewModel
-class NoteViewModel @Inject constructor(private val repository: NoteRepositoryImp) : ViewModel() {
+class NoteViewModel @Inject constructor(
+    private val repository: NoteRepositoryImp,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    val entityId = savedStateHandle.getStateFlow(key = Constants.ENTITY_ID, initialValue = 0)
+    val entityType : StateFlow<String?> = savedStateHandle.getStateFlow(key = Constants.ENTITY_TYPE, initialValue = null)
 
     private val _noteUiState = MutableStateFlow<NoteUiState>(NoteUiState.ShowProgressbar)
     val noteUiState: StateFlow<NoteUiState> get() = _noteUiState
@@ -29,10 +30,10 @@ class NoteViewModel @Inject constructor(private val repository: NoteRepositoryIm
     private val _isRefreshing = MutableStateFlow(false)
     val isRefreshing: StateFlow<Boolean> get() = _isRefreshing.asStateFlow()
 
-    fun refresh(type: String?, id: Int) {
+    fun refresh() {
         viewModelScope.launch {
             _isRefreshing.emit(true)
-            loadNote(type, id)
+            loadNote()
         }
     }
 
@@ -40,12 +41,13 @@ class NoteViewModel @Inject constructor(private val repository: NoteRepositoryIm
      * This method load the Notes.
      * Response: List<Note>
     </Note> */
-    fun loadNote(type: String?, id: Int) {
+    fun loadNote() {
+        Log.d("NoteScreendebug1", "id ${entityId.value} type ${entityType.value}")
         viewModelScope.launch {
             _noteUiState.value = NoteUiState.ShowProgressbar
             try {
                 val notes = withContext(Dispatchers.IO) {
-                    repository.getNotes(type, id)
+                    repository.getNotes(entityType.value, entityId.value)
                 }
                 if (notes.isNotEmpty()) {
                     _noteUiState.value = NoteUiState.ShowNote(notes)
