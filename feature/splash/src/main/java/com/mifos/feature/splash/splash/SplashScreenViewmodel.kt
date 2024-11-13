c7e03abdbb713cd659c32b71c1b40e03c7befd4f
Pronay sarker <pronaycoding@gmail.com>
2024-11-13 20:51:01 +0600

    splash cleanup
    fix errors

--- feature/splash/src/main/java/com/mifos/feature/splash/splash/SplashScreenViewmodel.kt
+++ feature/splash/src/main/java/com/mifos/feature/splash/splash/SplashScreenViewmodel.kt
@@ -1,6 +1,14 @@
+/*
+ * Copyright 2024 Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * See https://github.com/openMF/android-client/blob/master/LICENSE.md
+ */
 package com.mifos.feature.splash.splash
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mifos.core.datastore.PrefManager
@@ -14,7 +22,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SplashScreenViewmodel @Inject constructor(
-    private val prefManager: PrefManager
+    private val prefManager: PrefManager,
 ) : ViewModel() {
 
     private val _isAuthenticated = MutableStateFlow<Boolean?>(null)
@@ -28,5 +36,4 @@ class SplashScreenViewmodel @Inject constructor(
         delay(2000)
         _isAuthenticated.value = prefManager.isAuthenticated()
     }
-
 }
