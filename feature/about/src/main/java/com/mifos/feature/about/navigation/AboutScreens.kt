2b1d58b2368a426acb762f46bbb9c74021b3e92b
Darkeye14 <gyanopcon@gmail.com>
2024-11-29 23:22:17 +0530

    Applying spotless and detekt on about module (JIRA 280)

--- feature/about/src/main/java/com/mifos/feature/about/navigation/AboutScreens.kt
+++ feature/about/src/main/java/com/mifos/feature/about/navigation/AboutScreens.kt
@@ -1,8 +1,17 @@
+/*
+ * Copyright 2024 Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * See https://github.com/openMF/android-client/blob/master/LICENSE.md
+ */
 package com.mifos.feature.about.navigation
 
 /**
  * Created by Pronay Sarker on 18/08/2024 (2:42 PM)
  */
 sealed class AboutScreens(val route: String) {
-    data object AboutScreen :  AboutScreens(route = "about_screen_route")
+    data object AboutScreen : AboutScreens(route = "about_screen_route")
 }
--- feature/about/src/main/java/com/mifos/feature/about/navigation/AccountNavigation.kt
+++ feature/about/src/main/java/com/mifos/feature/about/navigation/AccountNavigation.kt
@@ -1,6 +1,14 @@
+/*
+ * Copyright 2024 Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * See https://github.com/openMF/android-client/blob/master/LICENSE.md
+ */
 package com.mifos.feature.about.navigation
 
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.mifos.feature.about.AboutScreen
@@ -9,11 +17,11 @@ import com.mifos.feature.about.AboutScreen
  * Created by Pronay Sarker on 10/08/2024 (7:56 AM)
  */
 fun NavGraphBuilder.aboutScreen(
-    onBackPressed: () -> Unit
+    onBackPressed: () -> Unit,
 ) {
     composable(AboutScreens.AboutScreen.route) {
         AboutScreen(
-            onBackPressed = onBackPressed
+            onBackPressed = onBackPressed,
         )
     }
 }
