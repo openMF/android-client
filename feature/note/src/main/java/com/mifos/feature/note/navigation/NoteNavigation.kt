345a950d2ddabcbf51c604bd4cb4347478c25735
Darkeye14 <gyanopcon@gmail.com>
2024-11-29 23:01:46 +0530

    applying spotless and detekt to note module (JIRA 279)

--- feature/note/src/main/java/com/mifos/feature/note/navigation/NoteNavigation.kt
+++ feature/note/src/main/java/com/mifos/feature/note/navigation/NoteNavigation.kt
@@ -1,7 +1,14 @@
+/*
+ * Copyright 2024 Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * See https://github.com/openMF/android-client/blob/master/LICENSE.md
+ */
 package com.mifos.feature.note.navigation
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
@@ -14,17 +21,17 @@ import com.mifos.feature.note.NoteScreen
  * Created by Pronay Sarker on 17/08/2024 (12:05 AM)
  */
 fun NavGraphBuilder.noteScreen(
-    onBackPressed: () -> Unit
+    onBackPressed: () -> Unit,
 ) {
     composable(
         route = NoteScreens.NoteScreen.route,
         arguments = listOf(
             navArgument(name = Constants.ENTITY_ID, builder = { NavType.IntType }),
-            navArgument(name = Constants.ENTITY_TYPE, builder = { NavType.StringType })
-        )
+            navArgument(name = Constants.ENTITY_TYPE, builder = { NavType.StringType }),
+        ),
     ) {
         NoteScreen(
-            onBackPressed = onBackPressed
+            onBackPressed = onBackPressed,
         )
     }
 }
