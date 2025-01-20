5be65aa932cb71252669e7e09d2a1a948a1caf6d
kapmaurya <kapilmaurya9@gmail.com>
2025-01-20 20:54:53 +0530

    datastore-migration-kmpl

---
+++ core/datastore/src/commonMain/kotlin/org.mifos.core.datastore/model/DefaultAccount.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2024 Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * See https://github.com/openMF/mobile-wallet/blob/master/LICENSE.md
+ */
+package org.mifos.core.datastore
+
+import kotlinx.serialization.Serializable
+import org.mifos.core.datastore.Parcelize
+
+@Serializable
+@Parcelize
+data class DefaultAccount(
+    val accountId: Long,
+    val accountNo: String,
+) : Parcelable {
+    companion object {
+        val DEFAULT = DefaultAccount(
+            accountId = 0,
+            accountNo = "",
+        )
+    }
+}
