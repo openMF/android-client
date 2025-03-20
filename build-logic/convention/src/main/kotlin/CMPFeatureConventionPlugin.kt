42d79a1379ca65668cc9641ab42a35c5c69e75e7
Pronay sarker <pronaycoding@gmail.com>
2025-03-21 00:18:36 +0600

    Fixed CMP plugins and dependencies

--- build-logic/convention/src/main/kotlin/CMPFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/CMPFeatureConventionPlugin.kt
@@ -8,17 +8,14 @@ class CMPFeatureConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             with(pluginManager) {
-                apply("mifos.kmp.libray")
+                apply("mifos.kmp.library")
                 apply("mifos.kmp.koin")
                 apply("org.jetbrains.kotlin.plugin.compose")
                 apply("org.jetbrains.compose")
             }
 
             dependencies {
-                add("commonMainImplementation", project(":core:ui"))
                 add("commonMainImplementation", project(":core:designsystem"))
-                add("commonMainImplementation", project(":core:testing"))
-                add("commonMainImplementation", project(":core:data"))
 
                 add("commonMainImplementation", libs.findLibrary("koin.compose").get())
                 add("commonMainImplementation", libs.findLibrary("koin.compose.viewmodel").get())
