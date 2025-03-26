a7f50d9988ab107bb2e98bb78dc249682926b139
Pronay sarker <pronaycoding@gmail.com>
2025-03-27 01:20:38 +0600

    removed commented out code

--- build-logic/convention/src/main/kotlin/CMPFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/CMPFeatureConventionPlugin.kt
@@ -15,10 +15,10 @@ class CMPFeatureConventionPlugin : Plugin<Project> {
             }
 
             dependencies {
-//                add("commonMainImplementation", project(":core:ui"))
+                add("commonMainImplementation", project(":core:ui"))
                 add("commonMainImplementation", project(":core:designsystem"))
-//                add("commonMainImplementation", project(":core:testing"))
-//                add("commonMainImplementation", project(":core:data"))
+                add("commonMainImplementation", project(":core:testing"))
+                add("commonMainImplementation", project(":core:data"))
 
                 add("commonMainImplementation", libs.findLibrary("koin.compose").get())
                 add("commonMainImplementation", libs.findLibrary("koin.compose.viewmodel").get())
