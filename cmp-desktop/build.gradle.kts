6ec6de022dcd636f75fb3c760a6ef88b34510a97
revanthkumarJ <jrevanth101@gmail.com>
2025-05-14 22:55:50 +0530

    updated

---
+++ cmp-desktop/build.gradle.kts
@@ -0,0 +1,81 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+kotlin {
+    jvm {
+        withJava()
+    }
+
+    jvmToolchain(17)
+
+    sourceSets {
+        jvmMain.dependencies {
+            implementation(projects.cmpShared)
+
+            implementation(libs.kotlinx.coroutines.swing)
+            implementation(compose.desktop.currentOs)
+            implementation(libs.jb.kotlin.stdlib)
+            implementation(libs.kotlin.reflect)
+
+            implementation(libs.koin.core)
+
+            implementation(compose.components.resources)
+        }
+    }
+}
+
+val appName: String = libs.versions.packageName.get()
+val packageNameSpace: String = libs.versions.packageNamespace.get()
+val appVersion: String = libs.versions.packageVersion.get()
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Exe, TargetFormat.Deb)
+            packageName = appName
+            packageVersion = appVersion
+            description = "Desktop Application"
+            copyright = "© 2024 Mifos Initiative. All rights reserved."
+            vendor = "Mifos Initiative"
+            licenseFile.set(project.file("../LICENSE"))
+            includeAllModules = true
+
+            macOS {
+                bundleID = packageNameSpace
+                dockName = appName
+                iconFile.set(project.file("icons/ic_launcher.icns"))
+                notarization {
+                    val providers = project.providers
+                    appleID.set(providers.environmentVariable("NOTARIZATION_APPLE_ID"))
+                    password.set(providers.environmentVariable("NOTARIZATION_PASSWORD"))
+                    teamID.set(providers.environmentVariable("NOTARIZATION_TEAM_ID"))
+                }
+            }
+
+            windows {
+                menuGroup = appName
+                shortcut = true
+                dirChooser = true
+                perUserInstall = true
+                iconFile.set(project.file("icons/ic_launcher.ico"))
+            }
+
+            linux {
+                modules("jdk.security.auth")
+                iconFile.set(project.file("icons/ic_launcher.png"))
+            }
+        }
+        buildTypes.release.proguard {
+            configurationFiles.from(file("compose-desktop.pro"))
+            obfuscate.set(true)
+            optimize.set(true)
+        }
+    }
+}
