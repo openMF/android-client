87ad8ddfc9ff45b9ac62b36a1762a39c2a040617
kapmaurya <kapilmaurya9@gmail.com>
2025-05-19 20:01:14 +0530

    cmp-migration note

--- feature/note/build.gradle.kts
+++ feature/note/build.gradle.kts
@@ -24,6 +24,7 @@ kotlin {
             api(projects.core.common)
             api(projects.core.model)
             api(projects.core.domain)
+            implementation(libs.kotlinx.serialization.json)
 
 
 
