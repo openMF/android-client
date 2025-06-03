a69fc838886919127658235e0362e90ee7f85cce
Nagarjuna <nagarjunabanda4@gmail.com>
2025-06-03 22:39:04 +0530

    feat(collection-sheet): migrated to cmp

---
+++ feature/collectionSheet/src/commonMain/kotlin/com/mifos/feature/individualCollectionSheet/individualCollectionSheet/ui/IndividualCollectionSheetScreen.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2024 Mifos Initiative
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * See https://github.com/openMF/android-client/blob/master/LICENSE.md
+ */
+
+package com.mifos.feature.individualCollectionSheet.individualCollectionSheet.ui
+
+import androidclient.feature.collectionsheet.generated.resources.Res
+import androidclient.feature.collectionsheet.generated.resources.feature_collection_sheet_individual_collection_sheet
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.mifos.core.designsystem.component.MifosScaffold
+import com.mifos.core.designsystem.component.MifosTabRow
+import com.mifos.core.designsystem.utility.TabContent
+import com.mifos.feature.individualCollectionSheet.newIndividualCollectionSheet.ui.NewIndividualCollectionSheetScreen
+import com.mifos.feature.individualCollectionSheet.savedIndividualCollectionSheet.ui.SavedIndividualCollectionSheetCompose
+import com.mifos.room.entities.collectionsheet.IndividualCollectionSheet
+import org.jetbrains.compose.resources.stringResource
+
+@Composable
+internal fun IndividualCollectionSheetScreen(
+    onBackPressed: () -> Unit,
+    onDetail: (String, IndividualCollectionSheet) -> Unit,
+) {
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    val pagerState = rememberPagerState(
+        pageCount = { IndividualCollectionSheetScreenContents.entries.size },
+    )
+
+    val tabContents = listOf(
+        TabContent(IndividualCollectionSheetScreenContents.NEW.name) {
+            NewIndividualCollectionSheetScreen(onDetail = onDetail)
+        },
+        TabContent(IndividualCollectionSheetScreenContents.SAVED.name) {
+            SavedIndividualCollectionSheetCompose()
+        },
+    )
+
+    MifosScaffold(
+        title = stringResource(Res.string.feature_collection_sheet_individual_collection_sheet),
+        onBackPressed = onBackPressed,
+        snackbarHostState = snackbarHostState,
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier.padding(paddingValues),
+        ) {
+            MifosTabRow(tabContents = tabContents, pagerState = pagerState)
+        }
+    }
+}
+
+enum class IndividualCollectionSheetScreenContents {
+    NEW,
+    SAVED,
+}
