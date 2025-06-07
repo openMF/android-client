d927f7ccdbf80b31f7e345b573b15391ce2c6686
revanthkumarJ <jrevanth101@gmail.com>
2025-06-07 22:01:46 +0530

    updated

--- cmp-navigation/src/commonMain/kotlin/cmp/navigation/di/KoinModules.kt
+++ cmp-navigation/src/commonMain/kotlin/cmp/navigation/di/KoinModules.kt
@@ -18,6 +18,7 @@ import com.mifos.core.network.di.NetworkModule
 import com.mifos.feature.activate.di.ActivateModule
 import com.mifos.feature.auth.di.AuthModule
 import com.mifos.feature.center.di.CenterModule
+import com.mifos.feature.checker.inbox.task.di.CheckerInboxTaskModule
 import com.mifos.feature.note.di.NoteModule
 import com.mifos.feature.search.di.SearchModule
 import com.mifos.feature.settings.di.SettingsModule
@@ -54,7 +55,7 @@ object KoinModules {
             ActivateModule,
             AuthModule,
             CenterModule,
-//            CheckerInboxTaskModule,
+            CheckerInboxTaskModule,
 //            ClientModule,
 //            CollectionSheetModule,
 //            DataTableModule,
