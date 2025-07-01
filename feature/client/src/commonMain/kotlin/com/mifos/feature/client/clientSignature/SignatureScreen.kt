85387dd769e3db1beed89c6400a1b80a4f8de684
revanthkumarJ <jrevanth101@gmail.com>
2025-07-01 22:57:51 +0530

    updated

--- feature/client/src/commonMain/kotlin/com/mifos/feature/client/clientSignature/SignatureScreen.kt
+++ feature/client/src/commonMain/kotlin/com/mifos/feature/client/clientSignature/SignatureScreen.kt
@@ -15,20 +15,52 @@ import androidclient.feature.client.generated.resources.Res
 import androidclient.feature.client.generated.resources.feature_client_failed_to_add_signature
 import androidclient.feature.client.generated.resources.feature_client_signature_gallery
 import androidclient.feature.client.generated.resources.feature_client_signature_reset
+import androidclient.feature.client.generated.resources.feature_client_signature_title
+import androidclient.feature.client.generated.resources.feature_client_signature_uploaded_successfully
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.boundsInRoot
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.mifos.core.common.utils.Constants
+import com.mifos.core.designsystem.component.DrawingState
+import com.mifos.core.designsystem.component.MifosCircularProgress
+import com.mifos.core.designsystem.component.MifosScaffold
+import com.mifos.core.designsystem.component.MifosSweetError
 import com.mifos.core.designsystem.icon.MifosIcons
 import com.mifos.core.ui.util.DevicePreview
+import com.niyajali.compose.sign.ComposeSign
+import com.niyajali.compose.sign.rememberSignatureState
 import io.github.vinceglb.filekit.PlatformFile
 import io.github.vinceglb.filekit.name
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.PreviewParameter
 import org.jetbrains.compose.ui.tooling.preview.PreviewParameterProvider
 import org.koin.compose.viewmodel.koinViewModel
+import kotlin.math.roundToInt
 
 @Composable
 internal fun SignatureScreen(
@@ -39,26 +71,72 @@ internal fun SignatureScreen(
     val state by viewmodel.signatureUiState.collectAsStateWithLifecycle()
 
     SignatureScreen(
-        state = state,
-        onBackPressed = onBackPressed,
-        uploadSignature = { file ->
-            viewmodel.createDocument(
-                Constants.ENTITY_TYPE_CLIENTS,
-                clientId,
-                file.name,
-                "Signature",
-                file,
-            )
-        },
+        state= state,
+        onBackPressed=onBackPressed,
+        uploadSignature = {}
     )
 }
 
 @Composable
-internal expect fun SignatureScreen(
+fun SignatureScreen(
     state: SignatureUiState,
     onBackPressed: () -> Unit,
     uploadSignature: (PlatformFile) -> Unit,
-)
+) {
+
+    var navigationSelectedItem by remember { mutableIntStateOf(0) }
+
+    val snackbarHostState = remember { SnackbarHostState() }
+    val signatureState = rememberSignatureState()
+
+
+    MifosScaffold(
+        title = stringResource(Res.string.feature_client_signature_title),
+        onBackPressed = onBackPressed,
+        snackbarHostState = snackbarHostState,
+        actions = {
+            IconButton(onClick = {
+
+            }) {
+                Icon(imageVector = MifosIcons.Upload, contentDescription = null)
+            }
+        },
+        bottomBar = {
+            NavigationBar {
+                BottomNavigationItem().bottomNavigationItems().forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        selected = index == navigationSelectedItem,
+                        label = { Text(item.label) },
+                        icon = { Icon(item.icon, contentDescription = item.label) },
+                        onClick = {
+                            navigationSelectedItem = index
+                            when (index) {
+                                0 -> {signatureState.clear()}
+                                1 -> {}
+                            }
+                        },
+                    )
+                }
+            }
+        },
+    ) { paddingValues ->
+        var size = remember { androidx.compose.ui.geometry.Size.Zero }
+        ComposeSign(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(4.dp)
+                .onGloballyPositioned {
+                    size = it.size.toSize()
+                },
+            state = signatureState,
+            strokeColor = MaterialTheme.colorScheme.onSurface,
+            backgroundColor = Color.Transparent,
+            showGrid = false,
+            onSignatureUpdate = {},
+        )
+    }
+}
 
 data class BottomNavigationItem(
     val label: String = "",
