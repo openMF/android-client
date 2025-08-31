fada042d8d739a3578db7fbe55ec88a1ac3f1b36
itsPronay <pronaycoding@gmail.com>
2025-08-31 14:38:10 +0600

    update

--- feature/client/src/commonMain/kotlin/com/mifos/feature/client/clientIdentitiesList/ClientIdentitiesListScreen.kt
+++ feature/client/src/commonMain/kotlin/com/mifos/feature/client/clientIdentitiesList/ClientIdentitiesListScreen.kt
@@ -1,2 +1,14 @@
 package com.mifos.feature.client.clientIdentitiesList
 
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import org.koin.compose.viewmodel.koinViewModel
+
+@Composable
+fun ClientIdentitiesListScreenRoute(
+    viewDocument : (Int) -> Unit,
+    createNewIdentities : (Int) -> Unit,
+    viewModel: ClientIdentitiesListViewModel = koinViewModel(),
+){
+    val state by viewModel.stateFlow.collectAsStateWithLifecycle()
+}
