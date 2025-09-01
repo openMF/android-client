54f3dc6288d98dd02b5f6cf7a573e99cd71f58a6
kalpesh <pawarkalpesh@proton.me>
2025-09-01 18:53:26 +0530

    typo fix

--- core/network/src/commonMain/kotlin/com/mifos/core/network/services/ClientService.kt
+++ core/network/src/commonMain/kotlin/com/mifos/core/network/services/ClientService.kt
@@ -284,7 +284,7 @@ interface ClientService {
         @Body payload: ProposeTransferRequest,
     ): HttpResponse
 
-    @POST("clients/{clientId}?command=updateSavingsdaAccount")
+    @POST("clients/{clientId}?command=updateSavingsAccount")
     suspend fun updateSavingsAccount(
         @Path("clientId") clientId: Int,
         @Body payload: UpdateSavingsAccountRequest,
