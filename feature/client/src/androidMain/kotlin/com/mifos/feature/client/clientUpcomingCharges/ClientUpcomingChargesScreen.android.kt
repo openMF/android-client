2ae4ce7a5c30cf9ffa775c3413f589771be1c3db
itsPronay <pronaycoding@gmail.com>
2025-09-05 16:14:15 +0600

    update

--- feature/client/src/androidMain/kotlin/com/mifos/feature/client/clientUpcomingCharges/ClientUpcomingChargesScreen.android.kt
+++ feature/client/src/androidMain/kotlin/com/mifos/feature/client/clientUpcomingCharges/ClientUpcomingChargesScreen.android.kt
@@ -76,7 +76,9 @@ actual fun ChargesListContent(
                     } else {
                         "N/A"
                     },
-                    due = charge.amount - charge.amountPaid, //todo check if its the right way to get due
+                    due = if (charge.amount != null && charge.amountPaid != null) {
+                        (charge.amount!! - charge.amountPaid!!).toString()
+                    } else "N/A", //todo check if its the right way to get due
                     paid = charge.amountPaid.toString(),
                     waived = charge.amountWaived.toString(),
                     outstanding = charge.amountOutstanding.toString(),
