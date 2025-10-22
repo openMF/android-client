39d4c0f2d09356a0c037be3c5b57d41bbf5ed4d2
Ankit kumar <ankitparyavarain03@gmail.com>
2025-10-22 12:44:12 +0530

    interest charges page modification

--- feature/client/src/commonMain/kotlin/com/mifos/feature/client/newFixedDepositAccount/pages/InterestChargesPage.kt
+++ feature/client/src/commonMain/kotlin/com/mifos/feature/client/newFixedDepositAccount/pages/InterestChargesPage.kt
@@ -13,19 +13,27 @@ import androidclient.feature.client.generated.resources.Res
 import androidclient.feature.client.generated.resources.step_interest
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun InterestPage(onNext: () -> Unit) {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(stringResource(Res.string.step_interest))
+        Text(stringResource(Res.string.step_interest),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.fillMaxWidth(0.4f)
+
+            )
         Spacer(Modifier.height(8.dp))
         Button(onClick = onNext) {
             Text("Next Button")
