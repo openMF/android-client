8fbf0942fc75362bef8effab4f0a2f90a6d8ac3d
itsPronay <pronaycoding@gmail.com>
2026-01-30 22:17:37 +0600

    Fixed broken navigation in LoanAccountSummary Screen

--- cmp-navigation/src/commonMain/kotlin/cmp/navigation/authenticated/AuthenticatedNavigation.kt
+++ cmp-navigation/src/commonMain/kotlin/cmp/navigation/authenticated/AuthenticatedNavigation.kt
@@ -18,7 +18,6 @@ import com.mifos.feature.activate.activateDestination
 import com.mifos.feature.checker.inbox.task.navigation.checkerInboxTaskNavGraph
 import com.mifos.feature.client.navigation.navigateClientDetailsScreen
 import com.mifos.feature.dataTable.navigation.dataTableNavGraph
-import com.mifos.feature.document.navigation.documentListScreen
 import com.mifos.feature.document.navigation.navigateToDocumentListScreen
 import com.mifos.feature.individualCollectionSheet.navigation.generateCollectionSheetScreen
 import com.mifos.feature.individualCollectionSheet.navigation.individualCollectionSheetNavGraph
@@ -58,8 +57,6 @@ internal fun NavGraphBuilder.authenticatedGraph(
         )
 
         checkerInboxTaskNavGraph(navController)
-
-        documentListScreen(onBackPressed = navController::popBackStack)
 
         dataTableNavGraph(
             navController = navController,
--- feature/client/src/commonMain/kotlin/com/mifos/feature/client/navigation/ClientNavigation.kt
+++ feature/client/src/commonMain/kotlin/com/mifos/feature/client/navigation/ClientNavigation.kt
@@ -86,7 +86,10 @@ import com.mifos.feature.client.savingsAccounts.navigateToClientSavingsAccountsR
 import com.mifos.feature.client.savingsAccounts.savingsAccountsDestination
 import com.mifos.feature.client.shareAccounts.navigateToShareAccountsScreen
 import com.mifos.feature.client.shareAccounts.shareAccountsDestination
+import com.mifos.feature.dataTable.navigation.dataTableRoute
+import com.mifos.feature.dataTable.navigation.navigateDataTableData
 import com.mifos.feature.dataTable.navigation.navigateToDataTable
+import com.mifos.feature.document.navigation.documentListScreen
 import com.mifos.feature.document.navigation.navigateToDocumentListScreen
 import com.mifos.feature.groups.navigation.navigateToGroupDetailsScreen
 import com.mifos.feature.loan.loanAccount.navigateToLoanAccountScreen
@@ -241,7 +244,10 @@ fun NavGraphBuilder.clientNavGraph(
             sharesAccounts = navController::navigateToShareAccountsScreen,
             fixedDepositAccounts = navController::navigateToFixedDepositAccountRoute,
             upcomingCharges = {
-                navController.navigateToClientUpcomingChargesRoute(it, Constants.ENTITY_TYPE_CLIENTS)
+                navController.navigateToClientUpcomingChargesRoute(
+                    it,
+                    Constants.ENTITY_TYPE_CLIENTS,
+                )
             },
         )
 
@@ -357,6 +363,13 @@ fun NavGraphBuilder.clientNavGraph(
             onMoreInfoClicked = navController::navigateToDataTable,
             onDocumentsClicked = navController::navigateToDocumentListScreen,
         )
+
+        dataTableRoute(
+            onBackPressed = navController::popBackStack,
+            onClick = navController::navigateDataTableData,
+        )
+
+        documentListScreen(onBackPressed = navController::popBackStack)
 
         savingsDestination(
             navController = navController,
