60f0b729760f9be23f6150a9fac88d16101f0314
Ambarish Manna <amanna1041@gmail.com>
2026-02-22 13:44:41 +0530

    refactor(search): use of xml strings instead of hardcoded values

--- feature/search/src/commonMain/kotlin/com/mifos/feature/search/components/SearchBox.kt
+++ feature/search/src/commonMain/kotlin/com/mifos/feature/search/components/SearchBox.kt
@@ -10,6 +10,7 @@
 package com.mifos.feature.search.components
 
 import androidclient.feature.search.generated.resources.Res
+import androidclient.feature.search.generated.resources.feature_search_all
 import androidclient.feature.search.generated.resources.feature_search_empty_input_field
 import androidclient.feature.search.generated.resources.feature_search_exact_match
 import androidclient.feature.search.generated.resources.feature_search_search_hint
@@ -83,7 +84,8 @@ internal fun SearchBox(
                     },
                     label = {
                         Text(
-                            text = state.selectedFilter?.label ?: "All",
+                            text = state.selectedFilter?.let { stringResource(it.labelRes) }
+                                ?: stringResource(Res.string.feature_search_all),
                             style = MaterialTheme.typography.bodyLarge,
                         )
                     },
