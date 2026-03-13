e88a5b9ddb1ecf524376bf8001182fdf68fda015
Ambarish Manna <amanna1041@gmail.com>
2026-03-13 20:23:17 +0530

    feat: loan note screen

--- feature/loan/src/commonMain/kotlin/com/mifos/feature/loan/loanAccountProfile/LoanAccountProfileScreen.kt
+++ feature/loan/src/commonMain/kotlin/com/mifos/feature/loan/loanAccountProfile/LoanAccountProfileScreen.kt
@@ -83,6 +83,7 @@ internal fun LoanAccountProfileScreen(
     navigateToTransactions: (Int) -> Unit,
     navigateToCharges: (Int) -> Unit,
     navigateToDocuments: (Int) -> Unit,
+    navigateToNotes: (Int) -> Unit,
     navigateToTransferScreen: (loanId: Int, accountNumber: String, clientId: Int, currencyCode: String, officeId: Int) -> Unit,
     navController: NavController,
     modifier: Modifier = Modifier,
@@ -119,6 +120,7 @@ internal fun LoanAccountProfileScreen(
                     LoanAccountProfileActionItem.Transactions -> navigateToTransactions(loanId)
                     LoanAccountProfileActionItem.Charges -> navigateToCharges(loanId)
                     LoanAccountProfileActionItem.Documents -> navigateToDocuments(loanId)
+                    LoanAccountProfileActionItem.Notes -> navigateToNotes(loanId)
                     else -> { }
                 }
             }
