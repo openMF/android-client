f13438702743d27ba79a7c5870fe5bb09eb7e67f
Gurnoor Singh Pannu <gurnxxrpannu@gmail.com>
2026-03-20 13:11:14 +0530

    spotless fix

--- feature/loan/src/commonMain/kotlin/com/mifos/feature/loan/loanTransaction/LoanTransactionsScreen.kt
+++ feature/loan/src/commonMain/kotlin/com/mifos/feature/loan/loanTransaction/LoanTransactionsScreen.kt
@@ -33,7 +33,6 @@ import androidclient.feature.loan.generated.resources.feature_loan_transaction_a
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
