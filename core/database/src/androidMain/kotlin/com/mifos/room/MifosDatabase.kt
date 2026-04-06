0470e66b35509d6186062836ee2ae1c7e90b7018
sahilshivekar <sahilshivekarce@gmail.com>
2026-04-07 00:24:59 +0530

    Add migrations for LoanWithAssociations & Timeline entities

--- core/database/src/androidMain/kotlin/com/mifos/room/MifosDatabase.kt
+++ core/database/src/androidMain/kotlin/com/mifos/room/MifosDatabase.kt
@@ -9,6 +9,7 @@
  */
 package com.mifos.room
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
@@ -151,7 +152,9 @@ import com.mifos.room.typeconverters.CustomTypeConverters
     ],
     version = MifosDatabase.VERSION,
     exportSchema = true,
-    autoMigrations = [],
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2),
+    ],
 )
 @TypeConverters(
     CustomTypeConverters::class,
@@ -169,6 +172,6 @@ actual abstract class MifosDatabase : RoomDatabase() {
     actual abstract val surveyDao: SurveyDao
 
     companion object {
-        const val VERSION = 1
+        const val VERSION = 2
     }
 }
