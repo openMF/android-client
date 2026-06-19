8306b663437975e5f5e3da71be11c81cc484dcb7
sahilshivekar <sahilshivekarce@gmail.com>
2026-06-19 15:15:39 +0530

    fix(database): enable Room schema export for future migrations

--- core/database/src/desktopMain/kotlin/com/mifos/room/MifosDatabase.kt
+++ core/database/src/desktopMain/kotlin/com/mifos/room/MifosDatabase.kt
@@ -150,7 +150,7 @@ import com.mifos.room.typeconverters.CustomTypeConverters
         PaymentTypeOptionEntity::class,
     ],
     version = MifosDatabase.VERSION,
-    exportSchema = false,
+    exportSchema = true,
     autoMigrations = [],
 )
 @TypeConverters(
